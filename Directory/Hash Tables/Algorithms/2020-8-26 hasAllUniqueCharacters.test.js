63cdeabd1fa9d551514656619bca831fe0c13cce
Austin Theriot <austinmtheriot@gmail.com>
2020-08-26 14:36:41 -0500

    replaced console logs with tests

---
+++ Directory/Hash Tables/Algorithms/2020-8-26 hasAllUniqueCharacters.test.js
@@ -0,0 +1,38 @@
+//implemented with a hash table:
+let hasAllUniqueCharacters = (string) => {
+	const hashTable = {};
+	for (let ch of string) {
+		if (hashTable[ch]) {
+			return false;
+		} else {
+			hashTable[ch] = true;
+		}
+	}
+	return true;
+};
+
+test('If string has all unique characters', () => {
+	expect(hasAllUniqueCharacters('abcdef')).toBe(true);
+	expect(hasAllUniqueCharacters('a')).toBe(true);
+	expect(hasAllUniqueCharacters('22')).toBe(false);
+	expect(hasAllUniqueCharacters('ZZZ')).toBe(false);
+});
+
+//implemented without a hash table:
+hasAllUniqueCharacters = (string) => {
+	if (string.length === 0) return true;
+	const array = string.split('').sort((a, b) => a - b);
+	for (let i = 1; i < array.length; i++) {
+		if (array[i] === array[i - 1]) {
+			return false;
+		}
+	}
+	return true;
+};
+
+test('If string has all unique characters', () => {
+	expect(hasAllUniqueCharacters('abcdef')).toBe(true);
+	expect(hasAllUniqueCharacters('a')).toBe(true);
+	expect(hasAllUniqueCharacters('22')).toBe(false);
+	expect(hasAllUniqueCharacters('ZZZ')).toBe(false);
+});
