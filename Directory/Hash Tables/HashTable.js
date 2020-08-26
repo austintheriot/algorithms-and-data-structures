eb8caef484aab1250a813d33a75797109e58b83c
Austin Theriot <austinmtheriot@gmail.com>
2020-08-26 14:00:58 -0500

    completed some tests

--- Directory/Hash Tables/HashTable.js
+++ Directory/Hash Tables/HashTable.js
@@ -1,8 +1,8 @@
 //implement a Hash Table that uses separate chaining for conflict resolution
 const LinkedList = require('../Linked Lists/SinglyLinkedList.solution');
 
-module.exports = class HashTable {
-	//_hash(key)
-	//get(key)
-	//set(key, value)
-};
+//_hash(key)
+//get(key)
+//set(key, value)
+
+module.exports = class HashTable {};
