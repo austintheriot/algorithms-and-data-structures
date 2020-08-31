127c61a45222438edff9686ed880a420b70f7e83
Austin Theriot <austinmtheriot@gmail.com>
2020-08-31 13:45:25 -0500

    implemented getBit solution

--- Directory/Bit Manipulation/Get Bit/getBit.solution.js
+++ Directory/Bit Manipulation/Get Bit/getBit.solution.js
@@ -1,4 +1,9 @@
-//Method 1:
+//Method 1 - returns 1 or 0
 module.exports = getBit = (number, i) => {
 	return (number >> i) & 1;
+};
+
+//Method 2 - return true or false
+getBitAlt = (number, i) => {
+	return (number & (1 << i)) != 0;
 };
--- Directory/Bit Manipulation/Get Bit/getBit.test.js
+++ Directory/Bit Manipulation/Get Bit/getBit.test.js
@@ -3,46 +3,46 @@ const getBit = require('./getBit.solution');
 test('Get bit (zero based),', () => {
 	//1 === 0001
 	let number = 1;
-	expect(getBit(number, 0)).toBe(1); //1s place
-	expect(getBit(number, 1)).toBe(0); //too far
+	expect(getBit(number, 0)).toBeTruthy(); //1s place
+	expect(getBit(number, 1)).toBeFalsy(); //too far
 
 	//2 === 0010
 	number = 2;
-	expect(getBit(number, 0)).toBe(0); //1s place
-	expect(getBit(number, 1)).toBe(1); //2s place
-	expect(getBit(number, 2)).toBe(0); //too far
+	expect(getBit(number, 0)).toBeFalsy(); //1s place
+	expect(getBit(number, 1)).toBeTruthy(); //2s place
+	expect(getBit(number, 2)).toBeFalsy(); //too far
 
 	//3 === 0011
 	number = 3;
-	expect(getBit(number, 0)).toBe(1); //1s place
-	expect(getBit(number, 1)).toBe(1); //2s place
-	expect(getBit(number, 2)).toBe(0); //too far
+	expect(getBit(number, 0)).toBeTruthy(); //1s place
+	expect(getBit(number, 1)).toBeTruthy(); //2s place
+	expect(getBit(number, 2)).toBeFalsy(); //too far
 
 	//4 === 0100
 	number = 4;
-	expect(getBit(number, 0)).toBe(0); //1s place
-	expect(getBit(number, 1)).toBe(0); //2s place
-	expect(getBit(number, 2)).toBe(1); //4s place
-	expect(getBit(number, 3)).toBe(0); //too far
+	expect(getBit(number, 0)).toBeFalsy(); //1s place
+	expect(getBit(number, 1)).toBeFalsy(); //2s place
+	expect(getBit(number, 2)).toBeTruthy(); //4s place
+	expect(getBit(number, 3)).toBeFalsy(); //too far
 
 	//5 === 0101
 	number = 5;
-	expect(getBit(number, 0)).toBe(1); //1s place
-	expect(getBit(number, 1)).toBe(0); //2s place
-	expect(getBit(number, 2)).toBe(1); //4s place
-	expect(getBit(number, 3)).toBe(0); //too far
+	expect(getBit(number, 0)).toBeTruthy(); //1s place
+	expect(getBit(number, 1)).toBeFalsy(); //2s place
+	expect(getBit(number, 2)).toBeTruthy(); //4s place
+	expect(getBit(number, 3)).toBeFalsy(); //too far
 
 	//6 === 0110
 	number = 6;
-	expect(getBit(number, 0)).toBe(0); //1s place
-	expect(getBit(number, 1)).toBe(1); //2s place
-	expect(getBit(number, 2)).toBe(1); //4s place
-	expect(getBit(number, 3)).toBe(0); //too far
+	expect(getBit(number, 0)).toBeFalsy(); //1s place
+	expect(getBit(number, 1)).toBeTruthy(); //2s place
+	expect(getBit(number, 2)).toBeTruthy(); //4s place
+	expect(getBit(number, 3)).toBeFalsy(); //too far
 
 	//7 === 0111
 	number = 7;
-	expect(getBit(number, 0)).toBe(1); //1s place
-	expect(getBit(number, 1)).toBe(1); //2s place
-	expect(getBit(number, 2)).toBe(1); //4s place
-	expect(getBit(number, 3)).toBe(0); //too far
+	expect(getBit(number, 0)).toBeTruthy(); //1s place
+	expect(getBit(number, 1)).toBeTruthy(); //2s place
+	expect(getBit(number, 2)).toBeTruthy(); //4s place
+	expect(getBit(number, 3)).toBeFalsy(); //too far
 });
