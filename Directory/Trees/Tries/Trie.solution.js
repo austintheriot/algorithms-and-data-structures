cb9b5dbee8efd77a9b9fe59941aadb79e2112699
Austin Theriot <austinmtheriot@gmail.com>
2020-09-01 14:56:52 -0500

    Trie tests

--- Directory/Trees/Tries/Trie.solution.js
+++ Directory/Trees/Tries/Trie.solution.js
@@ -1,6 +1,33 @@
-// class Node {
-//   constructor() {
-//     this.hashTable = {};
-//     isCompleteWord = false;
-//   }
-// }
+class Node {
+	constructor(value) {
+		this.value = value || null; //'c'
+		this.numberOfChildWords = 0;
+		this.children = {}; //{'a': Node}
+		this.isCompleteWord = false; //false
+	}
+}
+
+//add(string), add word to Trie
+//findWordCount(string), find the number of complete words with that string
+//getNode(string), get node of last string value
+
+module.exports = class Trie {
+	constructor() {
+		this.root = new Node(); //null root
+	}
+
+	add(string) {
+		let currentNode = this.root;
+		for (let i = 0; i < string.length; i++) {
+			currentNode.numberOfChildWords++;
+			let ch = string[i];
+			if (currentNode.children[ch]) {
+				currentNode = currentNode.children[ch];
+			} else {
+				currentNode.children[ch] = new Node(ch);
+				currentNode = currentNode.children[ch];
+			}
+		}
+		currentNode.isCompleteWord = true;
+	}
+};
---
+++ Directory/Trees/Tries/Trie.test.js
@@ -0,0 +1,9 @@
+describe('Trie', () => {
+	test('Should create an empty root node', () => {});
+
+	describe('add()', () => {
+		test('Should add nodes for each character to Trie', () => {});
+		test('Should add increment the word count of each character node touched', () => {});
+		test('Should add set isCompleteWord to true for the final letter', () => {});
+	});
+});
