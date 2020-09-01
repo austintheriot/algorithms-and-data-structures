8847e710a4852b6d02c5e25f2949ac05b4408193
Austin Theriot <austinmtheriot@gmail.com>
2020-09-01 10:32:07 -0500

    hash table practice

--- Directory/Hash Tables/HashTable.js
+++ Directory/Hash Tables/HashTable.js
@@ -1,8 +1,8 @@
 //implement a Hash Table that uses separate chaining for conflict resolution
 const LinkedList = require('../Linked Lists/SinglyLinkedList.solution');
 
-//_hash(key)
-//get(key)
-//set(key, value)
+//_hash
+//get
+//set
 
 module.exports = class HashTable {};
---
+++ Directory/Hash Tables/Previous Implementations/2020-9-1 HashTable.js
@@ -0,0 +1,38 @@
+//implement a Hash Table that uses separate chaining for conflict resolution
+const LinkedList = require('../Linked Lists/SinglyLinkedList.solution');
+
+//_hash
+//get
+//set
+
+module.exports = class HashTable {
+	constructor(length) {
+		this.array = new Array(length || 0);
+	}
+
+	_hash(key) {
+		let hash = 0;
+		for (let i = 0; i < key.length; i++) {
+			hash += key.charCodeAt(i) * i;
+		}
+		return hash % this.array.length;
+	}
+
+	get(key) {
+		const address = this._hash(key);
+		if (this.array[address]) {
+			return this.array[address].find((node) => node.data.key === key)?.data
+				.value;
+		}
+		return undefined;
+	}
+
+	set(key, value) {
+		const address = this._hash(key);
+		if (!this.array[address]) {
+			this.array[address] = new LinkedList();
+		}
+		this.array[address].prepend({ key, value });
+		return this;
+	}
+};
