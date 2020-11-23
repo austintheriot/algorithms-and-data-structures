80c0812358b1d59cf5df439d86d8a6b842fafc39
Austin Theriot <austinmtheriot@gmail.com>
2020-11-23 15:26:49 -0600

    set up depth first test

---
+++ Directory/Graphs/Depth First Search/depthFirstSearch.js
@@ -0,0 +1,6 @@
+module.exports = makeSearchFunction = (visitCallback) => {
+	return (node) => {
+		//implement a depth-first function here that
+		//calls the visitCallback() function for each node
+	};
+};
