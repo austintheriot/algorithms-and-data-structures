eb82017205217edcbc0809a1784e03c4854d768e
Austin Theriot <austinmtheriot@gmail.com>
2020-11-24 21:37:16 -0600

    isEven practice

---
+++ Directory/Bit Manipulation/Is Odd/isEven.test.js
@@ -0,0 +1,31 @@
+const isEven = require('./isEven.solution.js');
+
+for (let callback in isEven) {
+	describe(callback, () => {
+		it(`Should return 1 if even`, () => {
+			expect(isEven[callback](-14218)).toBeTruthy();
+			expect(isEven[callback](-254)).toBeTruthy();
+			expect(isEven[callback](-12)).toBeTruthy();
+			expect(isEven[callback](-6)).toBeTruthy();
+			expect(isEven[callback](0)).toBeTruthy();
+			expect(isEven[callback](2)).toBeTruthy();
+			expect(isEven[callback](6)).toBeTruthy();
+			expect(isEven[callback](24)).toBeTruthy();
+			expect(isEven[callback](158)).toBeTruthy();
+			expect(isEven[callback](1054130)).toBeTruthy();
+		});
+
+		it(`Should return 0 if odd`, () => {
+			expect(isEven[callback](-14217)).toBeFalsy();
+			expect(isEven[callback](-253)).toBeFalsy();
+			expect(isEven[callback](-11)).toBeFalsy();
+			expect(isEven[callback](-5)).toBeFalsy();
+			expect(isEven[callback](-1)).toBeFalsy();
+			expect(isEven[callback](1)).toBeFalsy();
+			expect(isEven[callback](5)).toBeFalsy();
+			expect(isEven[callback](21)).toBeFalsy();
+			expect(isEven[callback](159)).toBeFalsy();
+			expect(isEven[callback](1054133)).toBeFalsy();
+		});
+	});
+}
