d8e2f08347a3b8687066619674f2536d29b37da9
Austin Theriot <austinmtheriot@gmail.com>
2020-11-25 11:09:15 -0600

    added explanatory comments

--- Directory/Bit Manipulation/Addition/addition.solution.js
+++ Directory/Bit Manipulation/Addition/addition.solution.js
@@ -1,7 +1,10 @@
 module.exports = function addition(num1, num2) {
 	//"add" the numbers togethr using XOR
+	//this makes 1 + 1 = 0, and the carry integer is handled below
 	const sum = num1 ^ num2;
 	//create the "carry" by anding them
+	//the & creates a 1 where BOTH bits are 1 (where you would need to carry)
+	//then left shift by 1 to add in
 	const carry = (num1 & num2) << 1;
 	//if there is nothing to carry, good to go
 	//else, add the new "sum" and the new "carry"
