3c82be57c8b788245ac82ce86a3fece3d772811c
Austin Theriot <austinmtheriot@gmail.com>
2020-11-24 22:10:10 -0600

    created toggleBit files, erased solution in addition file

--- Directory/Bit Manipulation/Addition/addition.js
+++ Directory/Bit Manipulation/Addition/addition.js
@@ -1,5 +1,1 @@
-module.exports = function addition(num1, num2) {
-	const sum = num1 ^ num2;
-	const carry = (num1 & num2) << 1;
-	return !carry ? sum : addition(sum, carry);
-};
+module.exports = function addition(num1, num2) {};
