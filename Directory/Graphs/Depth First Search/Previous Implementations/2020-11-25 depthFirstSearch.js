e08cbf7d02042270d18323dc6599f26501cde6aa
Austin Theriot <austinmtheriot@gmail.com>
2020-11-25 10:56:51 -0600

    depth first search practice

---
+++ Directory/Graphs/Depth First Search/Previous Implementations/2020-11-25 depthFirstSearch.js
@@ -0,0 +1,14 @@
+module.exports = makeSearchFunction = (visitCallback) => {
+	return (search = (node) => {
+		//implement a depth-first function here that
+		//calls the visitCallback() function for each node
+		if (node === null) return;
+		visitCallback(node.data);
+		node.visited = true;
+		for (const child of node.children) {
+			if (child.visited === false) {
+				search(child);
+			}
+		}
+	});
+};
