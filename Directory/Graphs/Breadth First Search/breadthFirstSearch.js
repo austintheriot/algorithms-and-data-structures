4b4e0d0050ffbe19dd4aa12d4cb95b9020eeaeab
Austin Theriot <austinmtheriot@gmail.com>
2020-11-25 11:31:45 -0600

    created files for breadth-first search

---
+++ Directory/Graphs/Breadth First Search/breadthFirstSearch.js
@@ -0,0 +1,13 @@
+/* 
+interface Node {
+	visited: boolean;
+	children: Node[];
+} 
+*/
+
+module.exports = makeBreadthFirstSearchFunction = (visitCallback) => {
+	return (search = (node) => {
+		//implement a breadth-first function here that
+		//calls the visitCallback() function for each node
+	});
+};
---
+++ Directory/Graphs/Breadth First Search/breadthFirstSearch.test.js
@@ -0,0 +1,21 @@
+const exampleRootNode = require('../graphExample');
+const makeBreadthFirstSearchFunction = require('./breadthFirstSearch');
+
+let array;
+let visitCallback;
+let breadthFirstSearch;
+
+describe('Breadth-First Search (BFS)', () => {
+	beforeEach(() => {
+		array = [];
+		visitCallback = (data) => {
+			array.push(data);
+		};
+		breadthFirstSearch = makeBreadthFirstSearchFunction(visitCallback);
+	});
+
+	it('Should search by depth first', () => {
+		breadthFirstSearch(exampleRootNode);
+		expect(array).toEqual([0, 1, 4, 5, 3, 2]);
+	});
+});
