91bf82ad24e9f9fbe514638e342e26a1d4eb0b38
Austin Theriot <austinmtheriot@gmail.com>
2020-11-25 21:59:09 -0600

    created files for selection sort

---
+++ Directory/Sorting/Selection Sort/SelectionSort.about.md
@@ -0,0 +1,19 @@
+# Selection Sort
+
+## Table of Contents
+
+- [Selection Sort](#selection-sort)
+  - [Table of Contents](#table-of-contents)
+  - [About](#about)
+  - [Runtime](#runtime)
+
+## About
+
+
+
+## Runtime
+
+- Average: 
+- Worst: 
+- Memory: 
+
---
+++ Directory/Sorting/Selection Sort/selectionSort.test.js
@@ -0,0 +1,19 @@
+const selectionSort = require('./selectionSort');
+
+describe('Merge Sort', () => {
+	it('Should sort a given array from lowest highest', () => {
+		let arr = [10, 1, 4, 9, 7, 3];
+		expect(selectionSort(arr)).toEqual([1, 3, 4, 7, 9, 10]);
+
+		arr = [6, 4, 5, 8, 7, 9, 8];
+		expect(selectionSort(arr)).toEqual([4, 5, 6, 7, 8, 8, 9]);
+	});
+
+	it('Should not throw error on edge cases,', () => {
+		let arr = [];
+		expect(selectionSort(arr)).toEqual([]);
+
+		arr = [1];
+		expect(selectionSort(arr)).toEqual([1]);
+	});
+});
