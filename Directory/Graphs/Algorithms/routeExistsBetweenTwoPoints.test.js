abd22fa885d8dea7383ea27081763a8ae66b44ec
Austin Theriot <austinmtheriot@gmail.com>
2020-11-25 21:17:38 -0600

    bidirectional BFS

--- Directory/Graphs/Algorithms/routeExistsBetweenTwoPoints.test.js
+++ Directory/Graphs/Algorithms/routeExistsBetweenTwoPoints.test.js
@@ -5,18 +5,20 @@ const Node = require('../Graph');
 const resetGraph = (graph) => {
 	for (let node in graph) {
 		graph[node].visited = false;
+		graph[node].visitedI = false;
+		graph[node].visitedG = false;
 	}
 };
 
 //Breadth First Search (using a queue)
-const BFS = (nodeA, nodeB) => {
+const BFS = (xI, xG) => {
 	const queue = [];
-	nodeA.visited = true;
-	queue.push(nodeA);
+	xI.visited = true;
+	queue.push(xI);
 
 	while (queue.length !== 0) {
 		const dequeuedNode = queue.shift();
-		if (dequeuedNode === nodeB) return true;
+		if (dequeuedNode === xG) return true;
 		for (let child of dequeuedNode.children) {
 			if (child.visited === false) {
 				child.visited = true;
@@ -28,14 +30,14 @@ const BFS = (nodeA, nodeB) => {
 };
 
 //Depth First Search (using a stack)
-const DFS = (nodeA, nodeB) => {
+const DFS = (xI, xG) => {
 	const stack = [];
-	nodeA.visited = true;
-	stack.push(nodeA);
+	xI.visited = true;
+	stack.push(xI);
 
 	while (stack.length !== 0) {
 		const poppedNode = stack.pop();
-		if (poppedNode === nodeB) return true;
+		if (poppedNode === xG) return true;
 		for (let child of poppedNode.children) {
 			if (child.visited === false) {
 				child.visited = true;
@@ -45,6 +47,49 @@ const DFS = (nodeA, nodeB) => {
 	}
 	return false;
 };
+
+//Bidirectional BFS
+const bidirectionalSearch = (root1, root2) => {
+	const queue1 = [];
+	const queue2 = [];
+
+	queue1.push(root1);
+	root1.visited1 = true;
+	queue2.push(root2);
+	root2.visited2 = true;
+
+	while (queue1.length !== 0 && queue2.length !== 0) {
+		const dequeued1 = queue1.pop();
+		if (dequeued1.visited2) {
+			return true;
+		} else {
+			for (let childOf1 of dequeued1.children) {
+				if (!childOf1.visited1) {
+					childOf1.visited1 = true;
+					queue1.push(childOf1);
+				}
+			}
+		}
+		const dequeued2 = queue2.pop();
+		if (dequeued2.visited1) {
+			return true;
+		} else {
+			for (let childOf2 of dequeued2.children) {
+				if (!childOf2.visited2) {
+					childOf2.visited2 = true;
+					queue2.push(childOf2);
+				}
+			}
+		}
+	}
+	return false;
+};
+
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
 
 describe('Breadth First Search', () => {
 	describe('Directed Graph', () => {
@@ -140,6 +185,37 @@ describe('Depth First Search', () => {
 			resetGraph(undirGL);
 			undirGL.node35 = new Node(35);
 			expect(DFS(undirGL.node0, undirGL.node35)).toBe(false);
+		});
+	});
+});
+
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+describe('Bidirectional Search', () => {
+	describe('Undirected Graph', () => {
+		it('Should return true if a path exists between two nodes', () => {
+			resetGraph(undirGL);
+			expect(bidirectionalSearch(undirGL.node0, undirGL.node17)).toBe(true);
+
+			resetGraph(undirGL);
+			expect(bidirectionalSearch(undirGL.node19, undirGL.node15)).toBe(true);
+
+			//Create a new node that is connected
+			resetGraph(undirGL);
+			undirGL.node34 = new Node(34);
+			undirGL.node34.createUndirectedEdge(undirGL.node0);
+			expect(bidirectionalSearch(undirGL.node0, undirGL.node34)).toBe(true);
+		});
+
+		it('Should return false if a path does not exist between two nodes', () => {
+			//Create a new node that is not connected
+			resetGraph(undirGL);
+			undirGL.node35 = new Node(35);
+			expect(bidirectionalSearch(undirGL.node0, undirGL.node35)).toBe(false);
 		});
 	});
 });
