032981d4f54c7794c393ad2c49f22e6f1c4fec88
Austin Theriot <austinmtheriot@gmail.com>
2020-11-25 18:12:00 -0600

    route between two points practice

--- Directory/Graphs/Algorithms/routeExistsBetweenTwoPoints.test.js
+++ Directory/Graphs/Algorithms/routeExistsBetweenTwoPoints.test.js
@@ -1,5 +1,5 @@
-const directedGraphLarge = require('../Examples/directedGraphLarge');
-const undirectedGraphLarge = require('../Examples/undirectedGraphLarge');
+const dirGL = require('../Examples/directedGraphLarge');
+const undirGL = require('../Examples/undirectedGraphLarge');
 const Node = require('../Graph');
 
 const resetGraph = (graph) => {
@@ -8,17 +8,14 @@ const resetGraph = (graph) => {
 	}
 };
 
-//Breadth First Search
-let routeExistsBetweenTwoPoints = (nodeA, nodeB) => {
+//Breadth First Search (using a queue)
+const BFS = (nodeA, nodeB) => {
 	const queue = [];
 	nodeA.visited = true;
 	queue.push(nodeA);
 
-	let searches = 0;
-
 	while (queue.length !== 0) {
 		const dequeuedNode = queue.shift();
-		searches++;
 		if (dequeuedNode === nodeB) return true;
 		for (let child of dequeuedNode.children) {
 			if (child.visited === false) {
@@ -30,95 +27,119 @@ let routeExistsBetweenTwoPoints = (nodeA, nodeB) => {
 	return false;
 };
 
+//Depth First Search (using a stack)
+const DFS = (nodeA, nodeB) => {
+	const stack = [];
+	nodeA.visited = true;
+	stack.push(nodeA);
+
+	while (stack.length !== 0) {
+		const poppedNode = stack.pop();
+		if (poppedNode === nodeB) return true;
+		for (let child of poppedNode.children) {
+			if (child.visited === false) {
+				child.visited = true;
+				stack.push(child);
+			}
+		}
+	}
+	return false;
+};
+
 describe('Breadth First Search', () => {
 	describe('Directed Graph', () => {
 		it('Should return true if a path exists between two nodes', () => {
-			resetGraph(directedGraphLarge);
-			expect(
-				routeExistsBetweenTwoPoints(
-					directedGraphLarge.node0,
-					directedGraphLarge.node17
-				)
-			).toBe(true);
+			resetGraph(dirGL);
+			expect(BFS(dirGL.node0, dirGL.node17)).toBe(true);
 
-			resetGraph(directedGraphLarge);
-			expect(
-				routeExistsBetweenTwoPoints(
-					directedGraphLarge.node17,
-					directedGraphLarge.node0
-				)
-			).toBe(true);
+			resetGraph(dirGL);
+			expect(BFS(dirGL.node17, dirGL.node0)).toBe(true);
 
-			resetGraph(directedGraphLarge);
-			expect(
-				routeExistsBetweenTwoPoints(
-					directedGraphLarge.node16,
-					directedGraphLarge.node21
-				)
-			).toBe(true);
+			resetGraph(dirGL);
+			expect(BFS(dirGL.node16, dirGL.node21)).toBe(true);
 		});
 
 		it('Should return false if a path does not exist between two nodes', () => {
-			resetGraph(directedGraphLarge);
-			expect(
-				routeExistsBetweenTwoPoints(
-					directedGraphLarge.node20,
-					directedGraphLarge.node1
-				)
-			).toBe(false);
+			resetGraph(dirGL);
+			expect(BFS(dirGL.node20, dirGL.node1)).toBe(false);
 
-			resetGraph(directedGraphLarge);
-			expect(
-				routeExistsBetweenTwoPoints(
-					directedGraphLarge.node4,
-					directedGraphLarge.node9
-				)
-			).toBe(false);
+			resetGraph(dirGL);
+			expect(BFS(dirGL.node4, dirGL.node9)).toBe(false);
 		});
 	});
 
 	describe('Undirected Graph', () => {
 		it('Should return true if a path exists between two nodes', () => {
-			resetGraph(undirectedGraphLarge);
-			expect(
-				routeExistsBetweenTwoPoints(
-					undirectedGraphLarge.node0,
-					undirectedGraphLarge.node16
-				)
-			).toBe(true);
+			resetGraph(undirGL);
+			expect(BFS(undirGL.node0, undirGL.node16)).toBe(true);
 
-			resetGraph(undirectedGraphLarge);
-			expect(
-				routeExistsBetweenTwoPoints(
-					undirectedGraphLarge.node19,
-					undirectedGraphLarge.node15
-				)
-			).toBe(true);
+			resetGraph(undirGL);
+			expect(BFS(undirGL.node19, undirGL.node15)).toBe(true);
 
 			//Create a new node that is connected
-			resetGraph(undirectedGraphLarge);
-			undirectedGraphLarge.node34 = new Node(34);
-			undirectedGraphLarge.node34.createUndirectedEdge(
-				undirectedGraphLarge.node0
-			);
-			expect(
-				routeExistsBetweenTwoPoints(
-					undirectedGraphLarge.node0,
-					undirectedGraphLarge.node34
-				)
-			).toBe(true);
+			resetGraph(undirGL);
+			undirGL.node34 = new Node(34);
+			undirGL.node34.createUndirectedEdge(undirGL.node0);
+			expect(BFS(undirGL.node0, undirGL.node34)).toBe(true);
 		});
 
 		it('Should return false if a path does not exist between two nodes', () => {
 			//Create a new node that is not connected
-			resetGraph(undirectedGraphLarge);
-			undirectedGraphLarge.node35 = new Node(35);
-			expect(
-				routeExistsBetweenTwoPoints(
-					undirectedGraphLarge.node0,
-					undirectedGraphLarge.node35
-				)
-			).toBe(false);
+			resetGraph(undirGL);
+			undirGL.node35 = new Node(35);
+			expect(BFS(undirGL.node0, undirGL.node35)).toBe(false);
+		});
+	});
+});
+
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+describe('Depth First Search', () => {
+	describe('Directed Graph', () => {
+		it('Should return true if a path exists between two nodes', () => {
+			resetGraph(dirGL);
+			expect(DFS(dirGL.node0, dirGL.node17)).toBe(true);
+
+			resetGraph(dirGL);
+			expect(DFS(dirGL.node17, dirGL.node0)).toBe(true);
+
+			resetGraph(dirGL);
+			expect(DFS(dirGL.node16, dirGL.node21)).toBe(true);
+		});
+
+		it('Should return false if a path does not exist between two nodes', () => {
+			resetGraph(dirGL);
+			expect(DFS(dirGL.node20, dirGL.node1)).toBe(false);
+
+			resetGraph(dirGL);
+			expect(DFS(dirGL.node4, dirGL.node9)).toBe(false);
+		});
+	});
+
+	describe('Undirected Graph', () => {
+		it('Should return true if a path exists between two nodes', () => {
+			resetGraph(undirGL);
+			expect(DFS(undirGL.node0, undirGL.node17)).toBe(true);
+
+			resetGraph(undirGL);
+			expect(DFS(undirGL.node19, undirGL.node15)).toBe(true);
+
+			//Create a new node that is connected
+			resetGraph(undirGL);
+			undirGL.node34 = new Node(34);
+			undirGL.node34.createUndirectedEdge(undirGL.node0);
+			expect(DFS(undirGL.node0, undirGL.node34)).toBe(true);
+		});
+
+		it('Should return false if a path does not exist between two nodes', () => {
+			//Create a new node that is not connected
+			resetGraph(undirGL);
+			undirGL.node35 = new Node(35);
+			expect(DFS(undirGL.node0, undirGL.node35)).toBe(false);
 		});
 	});
 });
