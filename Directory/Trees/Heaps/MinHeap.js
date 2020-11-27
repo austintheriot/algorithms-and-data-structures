ad917a9c8c817be56cf25010fbc3740e76a699d2
Austin Theriot <austinmtheriot@gmail.com>
2020-11-27 15:12:55 -0600

    copied in all the non-tricky parts

--- Directory/Trees/Heaps/MinHeap.js
+++ Directory/Trees/Heaps/MinHeap.js
@@ -17,4 +17,59 @@
 // getMin(){}
 // extractMin(){}
 
-module.exports = class MinHeap {};
+module.exports = class MinHeap {
+	constructor() {
+		this.array = [];
+	}
+
+	getLeftChildIndex(index) {}
+
+	getRightChildIndex(index) {}
+
+	getParentIndex(index) {}
+
+	hasParent(index) {
+		const parentIndex = this.getParentIndex(index);
+		return this.array[parentIndex] === undefined ? false : true;
+	}
+
+	hasLeftChild(index) {
+		const leftChildIndex = this.getLeftChildIndex(index);
+		return this.array[leftChildIndex] === undefined ? false : true;
+	}
+
+	hasRightChild(index) {
+		const rightChildIndex = this.getRightChildIndex(index);
+		return this.array[rightChildIndex] === undefined ? false : true;
+	}
+
+	leftChild(index) {
+		const leftChildIndex = this.getLeftChildIndex(index);
+		return this.array[leftChildIndex];
+	}
+
+	rightChild(index) {
+		const rightChildIndex = this.getRightChildIndex(index);
+		return this.array[rightChildIndex];
+	}
+
+	parent(index) {
+		const parentIndex = this.getParentIndex(index);
+		return this.array[parentIndex];
+	}
+
+	swap(index1, index2) {
+		const temp = this.array[index1];
+		this.array[index1] = this.array[index2];
+		this.array[index2] = temp;
+		return this;
+	}
+
+	heapifyUp() {}
+
+	heapifyDown() {}
+
+	getMin() {}
+
+	extractMin() {}
+};
