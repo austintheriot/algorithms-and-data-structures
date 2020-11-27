8df7807b8393c50befc3ec944b9a5bced2ed4f7e
Austin Theriot <austinmtheriot@gmail.com>
2020-11-27 10:00:51 -0600

    improved selection sort implementation

--- Directory/Sorting/Selection Sort/selectionSort.js
+++ Directory/Sorting/Selection Sort/selectionSort.js
@@ -1,20 +1,19 @@
 module.exports = (originalArray) => {
 	const array = [...originalArray];
-	let startingIndex = 0;
-	while (startingIndex < array.length - 1) {
-		let smallestNumber = Infinity;
-		let smallestNumberIndex = -1;
-		for (let i = startingIndex; i < array.length; i++) {
-			if (array[i] < smallestNumber) {
-				smallestNumber = array[i];
-				smallestNumberIndex = i;
-			}
+
+	//Create a starting point that iterates through rray
+	for (let i = 0; i < array.length - 1; i++) {
+		let minNumIndex = i;
+
+		//Find minimum element
+		for (let j = i + 1; j < array.length; j++) {
+			if (array[j] < array[minNumIndex]) minNumIndex = j;
 		}
-		[array[startingIndex], array[smallestNumberIndex]] = [
-			array[smallestNumberIndex],
-			array[startingIndex],
-		];
-		startingIndex++;
+
+		//swap minimum element with current element
+		if (minNumIndex !== i) {
+			[array[i], array[minNumIndex]] = [array[minNumIndex], array[i]];
+		}
 	}
 	return array;
 };
