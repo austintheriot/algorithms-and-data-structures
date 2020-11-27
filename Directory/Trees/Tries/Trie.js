d75858aa404b46b7f98a4816df4db2930627afd2
Austin Theriot <austinmtheriot@gmail.com>
2020-11-27 14:42:00 -0600

    deleted existing implementation

--- Directory/Trees/Tries/Trie.js
+++ Directory/Trees/Tries/Trie.js
@@ -1,15 +1,5 @@
 // Implement a tree with the following functionalities:
 
-/* Node data:
-      value: string | null
-      numberOfChildWords: number
-      children = {
-      [index: string]: Node;
-      }
-      isCompleteWord: boolean
-      maxDepth: boolean;
-*/
-
 // getNode(string){}
 // add(string){}
 // isPrefix(string){}
@@ -27,53 +17,4 @@ class Node {
 	}
 }
 
-module.exports = class Trie {
-	constructor() {
-		this.root = new Node();
-	}
-
-	getNode(string) {
-		let currentNode = this.root;
-		for (let i = 0; i < string.length; i++) {
-			let ch = string[i];
-			if (currentNode.children[ch]) {
-				currentNode = currentNode.children[ch];
-			} else {
-				return undefined;
-			}
-		}
-		return currentNode;
-	}
-
-	add(string) {
-		if (this.getNode(string) && this.getNode(string).isCompleteWord)
-			return this;
-		let currentNode = this.root;
-		for (let i = 0; i < string.length; i++) {
-			const ch = string[i];
-			currentNode.numberOfChildWords++;
-			currentNode.maxDepth = Math.max(currentNode.maxDepth, string.length);
-			if (currentNode.children[ch]) {
-				currentNode = currentNode.children[ch];
-			} else {
-				currentNode.children[ch] = new Node(ch);
-				currentNode = currentNode.children[ch];
-			}
-		}
-		currentNode.isCompleteWord = true;
-		currentNode.numberOfChildWords++;
-	}
-
-	isPrefix(string) {
-		return this.getNode(string) ? true : false;
-	}
-	isWord(string) {
-		return this.getNode(string) ? this.getNode(string).isCompleteWord : false;
-	}
-	howManyWordsStartWith(string) {
-		return this.getNode(string) ? this.getNode(string).numberOfChildWords : 0;
-	}
-	longestStringLength() {
-		return this.root.maxDepth;
-	}
-};
+module.exports = class Trie {};
