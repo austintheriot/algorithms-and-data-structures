b178101b6bdc64604d21b42d48f4c2c338f48ea8
Austin Theriot <austinmtheriot@gmail.com>
2020-11-26 21:59:56 -0600

    implemented a rudimentary selection sort algorithm

--- Directory/Sorting/Selection Sort/selectionSort.js
+++ Directory/Sorting/Selection Sort/selectionSort.js
@@ -1,1 +1,20 @@
-module.exports = (originalArray) => {};
+module.exports = (originalArray) => {
+	const array = [...originalArray];
+	let startingIndex = 0;
+	while (startingIndex < array.length - 1) {
+		let smallestNumber = Infinity;
+		let smallestNumberIndex = -1;
+		for (let i = startingIndex; i < array.length; i++) {
+			if (array[i] < smallestNumber) {
+				smallestNumber = array[i];
+				smallestNumberIndex = i;
+			}
+		}
+		[array[startingIndex], array[smallestNumberIndex]] = [
+			array[smallestNumberIndex],
+			array[startingIndex],
+		];
+		startingIndex++;
+	}
+	return array;
+};
