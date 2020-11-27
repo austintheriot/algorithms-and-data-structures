431fb423b000a809d26aed2ffbc1d2686cceb288
Austin Theriot <austinmtheriot@gmail.com>
2020-11-27 17:33:18 -0600

    quick sort implementation

--- Directory/Sorting/Quick Sort/quickSort.js
+++ Directory/Sorting/Quick Sort/quickSort.js
@@ -1,1 +1,51 @@
-module.exports = function quickSort(originalArray) {};
+const swap = (array, index1, index2) => {
+	const temp = array[index1];
+	array[index1] = array[index2];
+	array[index2] = temp;
+
+	return array;
+};
+
+//Choose a pivot
+function partition(array, leftIndex, rightIndex) {
+	//get center index between the left and right indexes
+	const pivotIndex = Math.floor(leftIndex + (rightIndex - leftIndex) / 2);
+	//get center value
+	let pivotValue = array[pivotIndex];
+
+	//scan array for items that need to be swapped
+	while (leftIndex <= rightIndex) {
+		//find element on left that should be on right
+		while (array[leftIndex] < pivotValue) leftIndex++;
+
+		//find element on rightIndex that should be on leftIndex
+		while (array[rightIndex] > pivotValue) rightIndex--;
+
+		//if left and right pointers both found an element to swap,
+		//swap elements, and move leftIndex and rightIndex indices
+		if (leftIndex <= rightIndex) {
+			//swap elements
+			swap(array, leftIndex, rightIndex);
+			//ignore previously viewed/swapped elements and continue searching
+			leftIndex++;
+			rightIndex--;
+		}
+	}
+	return leftIndex;
+}
+
+module.exports = function quickSort(array, leftIndex, rightIndex) {
+	let pivotIndex = partition(array, leftIndex, rightIndex);
+
+	//sort left half
+	if (leftIndex < pivotIndex - 1) {
+		quickSort(array, leftIndex, pivotIndex - 1);
+	}
+
+	//sort right half
+	if (rightIndex > pivotIndex) {
+		quickSort(array, pivotIndex, rightIndex);
+	}
+
+	return array;
+};
--- Directory/Sorting/Quick Sort/quickSort.test.js
+++ Directory/Sorting/Quick Sort/quickSort.test.js
@@ -1,25 +1,53 @@
 const quickSort = require('./quickSort');
 
 describe('Merge Sort', () => {
-	it('Should sort a given array from lowest highest', () => {
+	it('Should sort in place', () => {
 		let arr = [-10, 1, -4, 9, 7, 3];
-		expect(quickSort(arr)).toEqual([-10, -4, 1, 3, 7, 9]);
-
-		arr = [-8.8, 0, 6, -1, 5, 8, 4, -5.4, 7, 9, 8];
-		expect(quickSort(arr)).toEqual([-8.8, -5.4, -1, 0, 4, 5, 6, 7, 8, 8, 9]);
+		expect(quickSort(arr, 0, arr.length - 1)).toBe(arr);
 	});
 
 	it('Should not throw error on edge cases,', () => {
 		let arr = [];
-		expect(quickSort(arr)).toEqual([]);
+		expect(quickSort(arr, 0, arr.length - 1)).toEqual([]);
 
 		arr = [1];
-		expect(quickSort(arr)).toEqual([1]);
+		expect(quickSort(arr, 0, arr.length - 1)).toEqual([1]);
 	});
 
-	it('Should not modify the original array,', () => {
-		let arr = [5, 4, 3, 2, 1, 0];
-		let sortedArr = quickSort(arr);
-		expect(sortedArr).not.toBe(arr);
+	it('Should sort a given array from lowest highest', () => {
+		let arr = [0, 5, 4, 3, 4, 6, 8];
+		expect(quickSort(arr, 0, arr.length - 1)).toEqual([0, 3, 4, 4, 5, 6, 8]);
+
+		arr = [0, 0, 1, 1, 2, 2, 3, 3, 5, 0];
+		expect(quickSort(arr, 0, arr.length - 1)).toEqual([
+			0,
+			0,
+			0,
+			1,
+			1,
+			2,
+			2,
+			3,
+			3,
+			5,
+		]);
+
+		arr = [-10, 1, -4, 9, 7, 3];
+		expect(quickSort(arr, 0, arr.length - 1)).toEqual([-10, -4, 1, 3, 7, 9]);
+
+		arr = [-8.8, 0, 6, -1, 5, 8, 4, -5.4, 7, 9, 8];
+		expect(quickSort(arr, 0, arr.length - 1)).toEqual([
+			-8.8,
+			-5.4,
+			-1,
+			0,
+			4,
+			5,
+			6,
+			7,
+			8,
+			8,
+			9,
+		]);
 	});
 });
