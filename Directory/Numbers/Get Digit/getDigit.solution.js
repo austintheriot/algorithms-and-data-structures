b21d80fea04dd41839424dbe641f3e6bcb269910
Austin Theriot <austinmtheriot@gmail.com>
2020-12-04 16:22:43 -0600

    added stipulation for out of range indices

--- Directory/Numbers/Get Digit/getDigit.solution.js
+++ Directory/Numbers/Get Digit/getDigit.solution.js
@@ -1,5 +1,16 @@
 //Get the ith digit from the right
+//return 0 for out of range indixes
+
+function countDigits(num) {
+	if (num === 0) return 1;
+	return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
 module.exports = function getDigit(num, i) {
+	//return 0 for out of range indixes
+	let digitCount = countDigits(num);
+	if (i < 0 || i > digitCount - 1) return 0;
+
 	//Example
 	//4321 -- getting the 1st index
 	//num / 10**1 -----> num / 10 ----> 432.1
--- Directory/Numbers/Get Digit/getDigit.test.js
+++ Directory/Numbers/Get Digit/getDigit.test.js
@@ -1,4 +1,4 @@
-const getDigit = require('./getDigit.js');
+const getDigit = require('./getDigit.solution');
 
 it('Should return the digit at the indicated index', () => {
 	expect(getDigit(4321, 0)).toBe(1);
@@ -12,4 +12,11 @@ it('Should work with negative numbers', () => {
 	expect(getDigit(-4321, 1)).toBe(2);
 	expect(getDigit(-4321, 2)).toBe(3);
 	expect(getDigit(-4321, 3)).toBe(4);
+});
+
+it('Should return 0 for indices out of range', () => {
+	expect(getDigit(4321, 4)).toBe(0);
+	expect(getDigit(4321, -1)).toBe(0);
+	expect(getDigit(-4321, 100)).toBe(0);
+	expect(getDigit(-4321, -100)).toBe(0);
 });
