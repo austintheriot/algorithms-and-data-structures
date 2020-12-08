136f466e130d7b45c35f09fd1e1bd0f1b9d9a056
Austin Theriot <austinmtheriot@gmail.com>
2020-12-08 14:30:26 -0600

    created test utilities

--- Directory/Sorting/Bubble Sort/bubbleSort.test.js
+++ Directory/Sorting/Bubble Sort/bubbleSort.test.js
@@ -1,12 +1,11 @@
 const bubbleSort = require('./bubbleSort');
+const testUtils = require('../../../utils/testUtils');
 
 describe('Bubble Sort', () => {
-	it('Should sort a given array from lowest highest', () => {
-		let arr = [10, 1, 4, 9, 7, 3];
-		expect(bubbleSort(arr)).toEqual([1, 3, 4, 7, 9, 10]);
-
-		arr = [6, 4, 5, 8, 7, 9, 8];
-		expect(bubbleSort(arr)).toEqual([4, 5, 6, 7, 8, 8, 9]);
+	it('Should sort a given array from lowest to highest', () => {
+		testUtils.testPositiveIntegerSorting(bubbleSort);
+		testUtils.testIntegerSorting(bubbleSort);
+		testUtils.testDecimalSorting(bubbleSort);
 	});
 
 	it('Should successfully sort edge cases,', () => {
---
+++ utils/testUtils.js
@@ -0,0 +1,66 @@
+const shuffle = require('../directory/Shuffling/fisherYatesShuffle.solution');
+
+/* HELPER FUNCTIONS /////////////////////////////////////////////////////////////////////////////// */
+
+//Wrapping our assertions in a try...catch block gives us more helpful Stack Traces / errors when a test fails
+function tryCatchWrap(sort, shuffledArray, sortedArray, functionName) {
+	try {
+		expect(sort(shuffledArray)).toEqual(sortedArray);
+	} catch (error) {
+		//Limit stack track to the line that includes the assertion name
+		Error.captureStackTrace(error, functionName);
+		throw error;
+	}
+}
+
+function makeArray(type, arrayLength) {
+	switch (type) {
+		case 'positiveInteger':
+			return new Array(arrayLength)
+				.fill(0)
+				.map(() => Math.floor(Math.random() * arrayLength));
+		case 'integer':
+			return new Array(arrayLength)
+				.fill(0)
+				.map(() => Math.floor(Math.random() * arrayLength) - arrayLength / 2);
+		case 'decimal':
+			return new Array(arrayLength)
+				.fill(0)
+				.map(() => Math.random() * arrayLength - arrayLength / 2);
+		default:
+			return new Array(arrayLength).fill(0).map(() => Math.random());
+	}
+}
+
+function createTestAssertion(arrayType, functionName) {
+	global[functionName] = function (sort, arrayLength = 5) {
+		const unsortedArray = makeArray(arrayType, arrayLength);
+		const sortedArray = [...unsortedArray].sort((a, b) => a - b);
+		tryCatchWrap(sort, unsortedArray, sortedArray, global[functionName]);
+	};
+	return global[functionName];
+}
+
+/* ASSERTION FUNCTIONS /////////////////////////////////////////////////////////////////////////////// */
+
+const assertSorted = function assertSorted(sort, sortedArray) {
+	let shuffledArray = shuffle([...sortedArray]);
+	tryCatchWrap(sort, shuffledArray, sortedArray, assertSorted);
+};
+
+const testPositiveIntegerSorting = createTestAssertion(
+	'positiveInteger', //make array
+	'testPositiveIntegerSorting' //make function
+);
+const testIntegerSorting = createTestAssertion('integer', 'testIntegerSorting');
+
+const testDecimalSorting = createTestAssertion('decimal', 'testDecimalSorting');
+
+/* EXPORTS /////////////////////////////////////////////////////////////////////////////// */
+
+module.exports = {
+	assertSorted,
+	testPositiveIntegerSorting,
+	testIntegerSorting,
+	testDecimalSorting,
+};
