dc7e4935b86bbe88d06af1581bc749b7105472aa
Austin Theriot <austinmtheriot@gmail.com>
2020-12-08 12:52:43 -0600

    selectionSort practice

---
+++ Directory/Sorting/Selection Sort/Previous Implementations/2020-12-8 selectionSort.js
@@ -0,0 +1,12 @@
+module.exports = (array) => {
+	for (let i = 0; i < array.length; i++) {
+		let minIndex = i;
+		for (let j = i + 1; j < array.length; j++) {
+			if (array[j] < array[minIndex]) minIndex = j;
+		}
+		if (minIndex !== i) {
+			[array[i], array[minIndex]] = [array[minIndex], array[i]];
+		}
+	}
+	return array;
+};
--- Directory/Sorting/Selection Sort/selectionSort.js
+++ Directory/Sorting/Selection Sort/selectionSort.js
@@ -1,1 +1,1 @@
-module.exports = (array) => {};
+module.exports = () => {};
