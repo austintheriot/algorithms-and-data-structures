c60d0d873dbfd1beba78eb01b6caeaa314e75f12
Austin Theriot <austinmtheriot@gmail.com>
2020-12-09 10:41:04 -0600

    fixed negative number bug

--- src/Numbers/Get Digit/getDigit.solution.js
+++ src/Numbers/Get Digit/getDigit.solution.js
@@ -9,5 +9,5 @@ function countDigits(num) {
 module.exports = function getDigit(num, i) {
 	if (i < 0 || i > countDigits(num) - 1) return 0;
 	let sign = num < 0 ? -1 : 1;
-	return Math.floor((num / 10 ** i) % 10) * sign;
+	return Math.floor((Math.abs(num) / 10 ** i) % 10) * sign;
 };
