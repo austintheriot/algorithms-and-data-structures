0dce5297ec9cb100d0bd11529eb77dbb6c5ff8b2
Austin Theriot <austinmtheriot@gmail.com>
2020-12-09 10:44:13 -0600

    added edge case utility

--- src/Sorting/Radix Sort/Decimal Sorting/radixSortDecimal.test.js
+++ src/Sorting/Radix Sort/Decimal Sorting/radixSortDecimal.test.js
@@ -1,24 +1,20 @@
-const radixSortDecimal = require('./radixSortDecimal.solution');
+const sort = require('./radixSortDecimal.solution');
 const testUtils = require('../../../../utils/testUtils');
 
 describe('Radix Sort', () => {
 	it('Should sort an array of numbers from lowest to highest', () => {
-		testUtils.testPositiveIntegerSorting(radixSortDecimal);
-		testUtils.testIntegerSorting(radixSortDecimal);
-		testUtils.testDecimalSorting(radixSortDecimal);
+		testUtils.testPositiveIntegerSorting(sort);
+		testUtils.testIntegerSorting(sort);
+		testUtils.testDecimalSorting(sort);
 	});
 
 	it('Should successfully sort edge cases,', () => {
-		let arr = [];
-		expect(radixSortDecimal(arr)).toEqual([]);
-
-		arr = [1];
-		expect(radixSortDecimal(arr)).toEqual([1]);
+		testUtils.testEdgeCases(sort);
 	});
 
 	it('Should not modify original array', () => {
 		arr = [5, 0, -5];
-		radixSortDecimal(arr);
+		sort(arr);
 		expect(arr).toEqual([5, 0, -5]);
 	});
 });
