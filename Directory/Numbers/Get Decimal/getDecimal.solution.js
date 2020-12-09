371b5f6819ee9e7b525e3fa2eb48c67b1264347d
Austin Theriot <austinmtheriot@gmail.com>
2020-12-08 18:58:06 -0600

    added comments and tests

--- Directory/Numbers/Get Decimal/getDecimal.solution.js
+++ Directory/Numbers/Get Decimal/getDecimal.solution.js
@@ -1,1 +1,31 @@
-module.exports = function getDecimal(num, i) {};
+//Get the ith digit from the right
+//return 0 for out of range indixes
+//return negative integer for negative numbers
+
+function extractDecimal(num) {
+	//extract the stuff after the decimal
+	num = Number(num.toPrecision(16).toString().split('.').pop());
+	//trim off any extra zeroes
+	while (num % 10 === 0) {
+		num /= 10;
+	}
+	return num;
+}
+
+function countDecimals(num) {
+	//no need to check integer for decimals
+	if (Number.isInteger(num)) return 0;
+	//get any numbers after the decimal point
+	const extractedDecimal = extractDecimal(Math.abs(num));
+	//count the number of digits
+	return Math.floor(Math.log10(extractedDecimal)) + 1;
+}
+
+function getDecimal(num, i) {
+	if (i < 0 || i > countDecimals(num) - 1) return 0;
+	const extractedDecimal = extractDecimal(num);
+	let sign = num < 0 ? -1 : 1;
+	return Math.floor((extractedDecimal / 10 ** i) % 10) * sign;
+}
+
+module.exports = getDecimal;
--- Directory/Numbers/Get Decimal/getDecimal.test.js
+++ Directory/Numbers/Get Decimal/getDecimal.test.js
@@ -1,17 +1,19 @@
 const getDecimal = require('./getDecimal');
 
 it('Should return the digit at the indicated decimal index', () => {
-	expect(getDecimal(0.4321, 0)).toBe(1);
-	expect(getDecimal(0.4321, 1)).toBe(2);
-	expect(getDecimal(0.4321, 2)).toBe(3);
-	expect(getDecimal(0.4321, 3)).toBe(4);
+	let num = 0.4321;
+	expect(getDecimal(num, 0)).toBe(1);
+	expect(getDecimal(num, 1)).toBe(2);
+	expect(getDecimal(num, 2)).toBe(3);
+	expect(getDecimal(num, 3)).toBe(4);
 });
 
 it('Should work with negative numbers', () => {
-	expect(getDecimal(-0.4321, 0)).toBe(-1);
-	expect(getDecimal(-0.4321, 1)).toBe(-2);
-	expect(getDecimal(-0.4321, 2)).toBe(-3);
-	expect(getDecimal(-0.4321, 3)).toBe(-4);
+	let num = -0.4321;
+	expect(getDecimal(num, 0)).toBe(-1);
+	expect(getDecimal(num, 1)).toBe(-2);
+	expect(getDecimal(num, 2)).toBe(-3);
+	expect(getDecimal(num, 3)).toBe(-4);
 });
 
 it('Should return 0 for indices out of range', () => {
@@ -19,4 +21,11 @@ it('Should return 0 for indices out of range', () => {
 	expect(getDecimal(0.4321, -1)).toBe(0);
 	expect(getDecimal(-0.4321, 100)).toBe(0);
 	expect(getDecimal(-0.4321, -100)).toBe(0);
+});
+
+it('Should work for large numbers', () => {
+	let num = 1234567.987654321;
+	expect(getDecimal(num, 0)).toBe(1);
+	expect(getDecimal(num, 5)).toBe(6);
+	expect(getDecimal(num, 8)).toBe(9);
 });
