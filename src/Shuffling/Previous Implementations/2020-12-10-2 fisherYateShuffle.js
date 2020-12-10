86b476866870e1535824d0aaeac65884369d513f
Austin Theriot <austinmtheriot@gmail.com>
2020-12-10 15:20:10 -0600

    fisherYatesShuffle practice

---
+++ src/Shuffling/Previous Implementations/2020-12-10-2 fisherYateShuffle.js
@@ -0,0 +1,8 @@
+//should modify the original array
+module.exports = function fisherYatesShuffle(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		let j = Math.floor(i + Math.random() * (arr.length - i));
+		[arr[i], arr[j]] = [arr[j], arr[i]];
+	}
+	return arr;
+};
