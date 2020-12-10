aa522573dd9429ee4849302de9994217db836b8e
Austin Theriot <austinmtheriot@gmail.com>
2020-12-10 16:28:42 -0600

    added test for even distribution

--- src/Shuffling/fisherYatesShuffle.test.js
+++ src/Shuffling/fisherYatesShuffle.test.js
@@ -1,4 +1,5 @@
 const shuffle = require('./fisherYatesShuffle');
+const shufflingUtils = require('../../utils/shufflingUtils');
 
 describe('Fisher-Yates Shuffle', () => {
 	it('Should shuffle an array', () => {
@@ -7,9 +8,13 @@ describe('Fisher-Yates Shuffle', () => {
 		expect(shuffle(array)).not.toEqual(arrayCopy);
 	});
 
-	it('Should modifiy the original array', () => {
+	it('Should shuffle in place (modify the original array)', () => {
 		let array = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
 		const shuffledArray = shuffle(array);
 		expect(shuffledArray).toBe(array);
+	});
+
+	it('Should produce an even distribution of randomness', () => {
+		shufflingUtils.assertEvenDistribution(shuffle);
 	});
 });
