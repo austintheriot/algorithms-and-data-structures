da864da5d09a480ece41ab899d9dddae6d703cff
Austin Theriot <austinmtheriot@gmail.com>
2020-12-11 16:15:53 -0600

    implemented sieve of eratosthenes in linear time

---
+++ src/Numbers/Sieve of Eratosthenes/Linear Time/sieveLinear.solution.js
@@ -0,0 +1,35 @@
+//This version of Erastothenes' Sieve works in linear time
+//by "cross out" each non-prime only one time
+//drawback is that it requires additionaly memory
+//of O(n) to store smallest prime factors
+
+export default function sieve(N) {
+	//smallest prime factor for of each index
+	//any number that remains with 0 is prime
+	const spf = new Array(N + 1).fill(0);
+
+	//final array of prime numbers
+	const primes = [];
+
+	for (let i = 2; i <= N; i++) {
+		//if it is prime
+		if (spf[i] === 0) {
+			spf[i] = i;
+			primes.push(i);
+		}
+
+		//iterate through primes up to the current number's lowest prime
+		//mark currentNumber * lowest prime as not prime (mark it's smallest prime factor)
+		for (
+			let j = 0;
+			j < primes.length && //iterate through array of found primes
+			primes[j] <= spf[i] && //only iterate primes up to the i's lowest prime
+			i * primes[j] <= N; //do not check beyond max
+			j++
+		) {
+			//set the smallest prime factor of the multiple
+			spf[i * primes[j]] = primes[j];
+		}
+	}
+	return primes;
+}
