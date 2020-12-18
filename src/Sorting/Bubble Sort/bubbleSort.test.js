62fecdbbfe836a2389ba5bf3b6c27ea332bfa125
Austin Theriot <austinmtheriot@gmail.com>
2020-12-18 09:38:35 -0600

    added ESModules imports

--- src/Sorting/Bubble Sort/bubbleSort.test.js
+++ src/Sorting/Bubble Sort/bubbleSort.test.js
@@ -1,25 +1,25 @@
-const bubbleSort = require('./bubbleSort');
-const sortingUtils = require('../../utils/sortingUtils');
+import sort from './bubbleSort';
+import * as sortingUtils from '../../utils/sortingUtils';
 
 describe('Bubble Sort', () => {
 	it('Should sort a given array from lowest to highest', () => {
-		sortingUtils.testPositiveIntegerSorting(bubbleSort);
-		sortingUtils.testIntegerSorting(bubbleSort);
-		sortingUtils.testDecimalSorting(bubbleSort);
+		sortingUtils.testPositiveIntegerSorting(sort);
+		sortingUtils.testIntegerSorting(sort);
+		sortingUtils.testDecimalSorting(sort);
 	});
 
 	it('Should successfully sort edge cases,', () => {
 		let arr = [];
-		expect(bubbleSort(arr)).toEqual([]);
+		expect(sort(arr)).toEqual([]);
 
 		arr = [1];
-		expect(bubbleSort(arr)).toEqual([1]);
+		expect(sort(arr)).toEqual([1]);
 	});
 
 	it('Should modify the original array,', () => {
 		let arr = [5, 4, 3, 2, 1, 0];
 		let arrCopy = [...arr];
-		let sortedArr = bubbleSort(arr);
+		let sortedArr = sort(arr);
 		expect(sortedArr).toBe(arr);
 		expect(arr).not.toEqual(arrCopy);
 	});
