c7c3c29ade32bd4fc8419f596728ee7115f14759
Austin Theriot <austinmtheriot@gmail.com>
2020-12-18 17:39:09 -0600

    --> ESModules

--- src/Queues/Queue.js
+++ src/Queues/Queue.js
@@ -1,10 +1,9 @@
 //implement a simple queue with the following functionalities:
-//use a linked list
 //return null for nonexistent peek() calls, etc.
 //add(){}
 //remove(){}
 //peek(){}
 //isEmpty(){}
-const LinkedList = require('../Linked Lists/DoublyLinkedList.solution');
 
-module.exports = class Queue {};
+//less performant but simpler
+export default class Queue {}
