ad2aaab9b6bccb8cc6ca70b1336fa54732245a7a
Austin Theriot <austinmtheriot@gmail.com>
2020-12-18 18:16:28 -0600

    bfs practice

--- src/Searching/Breadth First Search/breadthFirstSearch.js
+++ src/Searching/Breadth First Search/breadthFirstSearch.js
@@ -9,20 +9,6 @@ import Queue from '../../Queues/Queue.solution';
 
 export default function makeSearch(visitCallback) {
 	return function search(node) {
-		const queue = new Queue();
-		queue.add(node);
-
-		while (!queue.isEmpty()) {
-			const dequeuedNode = queue.remove();
-			dequeuedNode.visited = true;
-			visitCallback(dequeuedNode);
-
-			for (let child of dequeuedNode.children) {
-				if (!child.visited) {
-					child.visited = true;
-					queue.add(child);
-				}
-			}
-		}
+		//implement search here
 	};
 }
