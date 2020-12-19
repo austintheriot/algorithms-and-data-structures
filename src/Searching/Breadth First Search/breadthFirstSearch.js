64be58df0fb24403c73c9379d9b38ec4b863b061
Austin Theriot <austinmtheriot@gmail.com>
2020-12-18 18:15:35 -0600

    --> ESModules

--- src/Searching/Breadth First Search/breadthFirstSearch.js
+++ src/Searching/Breadth First Search/breadthFirstSearch.js
@@ -7,8 +7,22 @@ interface Node {
 
 import Queue from '../../Queues/Queue.solution';
 
-export default (visitCallback) => {
-	return (search = (root) => {
-		//implement here
-	});
-};
+export default function makeSearch(visitCallback) {
+	return function search(node) {
+		const queue = new Queue();
+		queue.add(node);
+
+		while (!queue.isEmpty()) {
+			const dequeuedNode = queue.remove();
+			dequeuedNode.visited = true;
+			visitCallback(dequeuedNode);
+
+			for (let child of dequeuedNode.children) {
+				if (!child.visited) {
+					child.visited = true;
+					queue.add(child);
+				}
+			}
+		}
+	};
+}
--- src/Searching/Breadth First Search/breadthFirstSearch.test.js
+++ src/Searching/Breadth First Search/breadthFirstSearch.test.js
@@ -1,20 +1,14 @@
 import rootNode from '../../Graphs/Examples/directedGraphSmall';
 import makeSearch from './breadthFirstSearch';
 
-let array;
-let visitCallback;
-let breadthFirstSearch;
-
 describe('Breadth-First Search (BFS)', () => {
-	beforeEach(() => {
-		array = [];
-		visitCallback = (node) => {
+	it('Should search by depth first', () => {
+		const array = [];
+		const visitCallback = (node) => {
 			array.push(node.data);
 		};
-		breadthFirstSearch = makeSearch(visitCallback);
-	});
+		const breadthFirstSearch = makeSearch(visitCallback);
 
-	it('Should search by depth first', () => {
 		breadthFirstSearch(rootNode);
 		expect(array).toEqual([0, 1, 4, 5, 3, 2]);
 	});
