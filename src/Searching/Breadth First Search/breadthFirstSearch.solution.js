c0c23e5ed4f6a245e067c9526ac07f93405ade73
Austin Theriot <austinmtheriot@gmail.com>
2020-12-18 18:15:49 -0600

    --> ESModules

--- src/Searching/Breadth First Search/breadthFirstSearch.solution.js
+++ src/Searching/Breadth First Search/breadthFirstSearch.solution.js
@@ -5,31 +5,30 @@ interface Node {
 } 
 */
 
-const Queue = require('../../Queues/Queue.solution');
+import Queue from '../../Queues/Queue.solution';
 
-export default function makeBreadthFirstSearchFunction(visitCallback) {
-	return (search = (root) => {
+export default function makeSearch(visitCallback) {
+	return function search(node) {
 		//THIS DOES *NOT* REQUIRE A RECURSIVE SOLUTION
 		//"visit" a node and add to the queue
 		//do not "process" the node until it is taken from the queue
 
 		const queue = new Queue(); //queue keeps track of the proper order
-
-		//"visit" the root node
-		root.visited = true;
-		queue.add(root);
+		queue.add(node);
 
 		//start taking nodes from the queue and processing them
 		while (!queue.isEmpty()) {
-			const queuedNode = queue.remove(); //take next node from the front of the queue
-			visitCallback(queuedNode.data);
+			const dequeuedNode = queue.remove();
+			dequeuedNode.visited = true;
+			visitCallback(dequeuedNode);
 
-			for (let node of queuedNode.children) {
-				if (node.visited === false) {
-					node.visited = true;
-					queue.add(node);
+			//visit each of the node's children and add them to the queue
+			for (let child of dequeuedNode.children) {
+				if (!child.visited) {
+					child.visited = true;
+					queue.add(child);
 				}
 			}
 		}
-	});
+	};
 }
