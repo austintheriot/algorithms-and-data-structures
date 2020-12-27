03a74a37d16c40e820d28c1d628e2ebaac471e99
Austin Theriot <austinmtheriot@gmail.com>
2020-12-27 17:58:07 -0600

    created doubly linked list blackbox test

--- src/Linked Lists/Doubly Linked List/DoublyLinkedList.blackbox.test.js
+++ src/Linked Lists/Doubly Linked List/DoublyLinkedList.blackbox.test.js
@@ -1,4 +1,4 @@
-const LinkedList = require('./DoublyLinkedList');
+import LinkedList from './DoublyLinkedList.solution';
 
 //return undefined for out of range indexes
 
@@ -17,18 +17,17 @@ describe('LinkedList (Black Box Test)', () => {
 		linkedList.prepend(100).prepend(50).prepend(0);
 
 		expect(linkedList.get(-1)).toBeUndefined();
-		expect(linkedList.get(0)).toBe(0);
-		expect(linkedList.get(1)).toBe(50);
-		expect(linkedList.get(2)).toBe(100);
+		expect(linkedList.get(0)?.data).toBe(0);
+		expect(linkedList.get(1)?.data).toBe(50);
+		expect(linkedList.get(2)?.data).toBe(100);
 		expect(linkedList.get(3)).toBeUndefined();
 
 		//use plain value
-		expect(linkedList.find(0)).toBeTruthy();
-		expect(linkedList.find(0).data).toBe(0);
-		expect(linkedList.find(50)).toBeTruthy();
-		expect(linkedList.find(0).data).toBe(50);
-		expect(linkedList.find(200)).toBeTruthy();
-		expect(linkedList.find(0).data).toBe(200);
+		expect(linkedList.find(-100)).toBeUndefined();
+		expect(linkedList.find(0)?.data).toBe(0);
+		expect(linkedList.find(50)?.data).toBe(50);
+		expect(linkedList.find(100)?.data).toBe(100);
+		expect(linkedList.find(200)).toBeUndefined();
 
 		//use search term
 		expect(linkedList.find((node) => node)).toBeTruthy();
@@ -38,20 +37,20 @@ describe('LinkedList (Black Box Test)', () => {
 		//out of range deletions
 		linkedList.delete(-1).delete(5).delete(100);
 		expect(linkedList.get(-1)).toBeUndefined();
-		expect(linkedList.get(0)).toBe(0);
-		expect(linkedList.get(1)).toBe(50);
-		expect(linkedList.get(2)).toBe(100);
+		expect(linkedList.get(0)?.data).toBe(0);
+		expect(linkedList.get(1)?.data).toBe(50);
+		expect(linkedList.get(2)?.data).toBe(100);
 		expect(linkedList.get(3)).toBeUndefined();
 
 		//in range deletions
 		linkedList.delete(2);
 		expect(linkedList.get(2)).toBeUndefined();
-		expect(linkedList.get(1)).toBe(50);
-		expect(linkedList.get(0)).toBe(0);
+		expect(linkedList.get(1)?.data).toBe(50);
+		expect(linkedList.get(0)?.data).toBe(0);
 		linkedList.delete(1);
 		expect(linkedList.get(2)).toBeUndefined();
 		expect(linkedList.get(1)).toBeUndefined();
-		expect(linkedList.get(0)).toBe(0);
+		expect(linkedList.get(0)?.data).toBe(0);
 		linkedList.delete(0);
 		expect(linkedList.get(2)).toBeUndefined();
 		expect(linkedList.get(1)).toBeUndefined();
