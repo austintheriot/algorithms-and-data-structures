3d0839e67e92303a68afa9f29075ad1ba98707ce
Austin Theriot <austinmtheriot@gmail.com>
2020-12-27 19:22:26 -0600

    fixed typo

--- src/Bit Manipulation/Update Bit/updateBit.test.js
+++ src/Bit Manipulation/Update Bit/updateBit.test.js
@@ -2,7 +2,7 @@ const updateBit = require('./updateBit');
 
 it('Update bit,', () => {
 	//7 === 0111
-	number = 0b0111;
+	const number = 0b0111;
 	expect(updateBit(number, 0, 0)).toBe(0b0110);
 	expect(updateBit(number, 1, 0)).toBe(0b0101);
 	expect(updateBit(number, 2, 0)).toBe(0b0011);
