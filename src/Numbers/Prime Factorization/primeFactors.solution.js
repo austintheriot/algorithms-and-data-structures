12c141cc930cdd3e909e8aacfdf51fea514593e9
Austin Theriot <austinmtheriot@gmail.com>
2020-12-29 11:20:33 -0600

    clarified runtime complexity

--- src/Numbers/Prime Factorization/primeFactors.solution.js
+++ src/Numbers/Prime Factorization/primeFactors.solution.js
@@ -8,11 +8,18 @@
     }; 
 
     In other words, 12345 = (3 ^ 1) * (5 ^ 1) * (823 ^ 1);
-
-    Runtime complexity for this function is O(sqrt(n)), 
-    since the greatest prime factor of a number will only 
-    only ever be as high as that number's square root.
     
+    Best possible runtime is O(k) where k is the number of prime factors in a number.
+      This occurs when a number is a product of 2s (i.e. 2, 4, 8, 16, 32, 64, 128, etc.)
+      128 = 2 ^ 7. The divisor is not incremented, and num is divided 7 times, so the runtime is O(7).
+
+    Worst possible runtime complexity for this function is O(sqrt(n)).
+      This occurs when a number is a perfect square of primes. 
+      The divisor is incremented until it is equal to the square root of the number,
+      and then the number is only divided by the divisor twice.
+
+      The greatest prime factor of a number will only 
+      only ever be as high as that number's square root.
 */
 
 export default function primeFactors(num) {
