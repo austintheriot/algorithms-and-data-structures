7c5694326e733fd6058af5a2f2bc692368fc2e5f
Austin Theriot <austinmtheriot@gmail.com>
2020-12-29 14:28:27 -0600

    renamed parameter

--- src/Arrays/Rotate Array/rotateArray.js
+++ src/Arrays/Rotate Array/rotateArray.js
@@ -1,4 +1,4 @@
 //rotate to the right [0, 1, 2] by 1 ---> [2, 0, 1]
 //allow rotations greater than array length
 
-export default function rotateArray(array, originalRotation) {}
+export default function rotateArray(array, rotation) {}
