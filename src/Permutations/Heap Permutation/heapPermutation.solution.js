8895a848135ade9d0e7204ea29c8e2e8b2dda8ce
Austin Theriot <austinmtheriot@gmail.com>
2020-12-29 08:55:29 -0600

    refactored solution

--- src/Permutations/Heap Permutation/heapPermutation.solution.js
+++ src/Permutations/Heap Permutation/heapPermutation.solution.js
@@ -1,36 +1,36 @@
-function swap(a, b, arr) {
+function swap(arr, a, b) {
 	var tmp = arr[a];
 	arr[a] = arr[b];
 	arr[b] = tmp;
 }
 
-//SHOULD manipulate original array
-function generate(A, n, permutations) {
-	//Escape case
-	if (n == 1) {
-		//Make a copy at each iteration, since the array is modified in place
-		return permutations.push(A.slice());
-	}
-
-	for (let i = 0; i < n; i++) {
-		//Generate permutations with nth unaltered
-		//Initially n == length(A)
-		generate(A, n - 1, permutations);
-
-		//if even
-		if (n % 2 === 0) {
-			swap(i, n - 1, A);
+export default function getPermutations(arr) {
+	//SHOULD manipulate original array
+	function generate(n, arr, answerArr) {
+		//recursive escape case
+		if (n === 1) {
+			answerArr.push(arr.slice());
+			return;
 		}
 
-		//if odd
-		else {
-			swap(0, n - 1, A);
-		}
-	}
-}
+		//generate permutations with nth unaltered
+		//initially n == length(A)
+		generate(n - 1, arr, answerArr);
 
-export default function permutationArr(arr) {
-	const permutations = [];
-	generate(arr, arr.length, permutations);
-	return permutations;
+		for (let i = 0; i < n - 1; i++) {
+			//if even
+			if (n % 2 === 0) swap(arr, i, n - 1);
+			//if odd
+			else swap(arr, 0, n - 1);
+			//generate permutation with newly swapped array
+			generate(n - 1, arr, answerArr);
+		}
+
+		return answerArr;
+	}
+
+	//run function
+	const answer = [];
+	generate(arr.length, arr, answer);
+	return answer;
 }
