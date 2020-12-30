4590c21d6876b5a363350f7c590031dbc534cc12
Austin Theriot <austinmtheriot@gmail.com>
2020-12-30 10:18:33 -0600

    updated solution

--- src/Numbers/Sieve of Eratosthenes/Standard/sieve.solution.js
+++ src/Numbers/Sieve of Eratosthenes/Standard/sieve.solution.js
@@ -1,24 +1,22 @@
-function sieve(max) {
+export default function sieve(max) {
 	const primes = [];
-	const numbers = new Array(max + 1).fill(true);
+	const isPrime = new Array(max + 1).fill(true);
 
-	numbers[0] = numbers[1] = false;
+	isPrime[0] = isPrime[1] = false;
 
 	for (let i = 2; i <= max; i++) {
-		if (numbers[i] === true) {
+		if (isPrime[i]) {
 			primes.push(i); //hasn't already been ruled out
 
 			//mark multiples of prime as not prime
 			//The following code MUST go inside the if block
 			//(or else will run in n * log(n) time and not n * log(log(n)) time)
-			let j = i * i; //this is an optimzation: can normally start with j = 2 * i
-			while (j <= max) {
-				numbers[j] = false;
-				j += i; //increment by multiples
+			//j normally starts at i * 2, but you can optimize by starting at i ** 2
+			for (let j = i ** 2; j <= max; j += i) {
+				isPrime[j] = false;
 			}
 		}
 	}
+
 	return primes;
 }
-
-module.exports = sieve;
