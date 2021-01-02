9fe71879310fde9816542bd458c01e3d57268ac4
Austin Theriot <austinmtheriot@gmail.com>
2021-01-02 15:01:20 -0600

    every possible combination implementation

---
+++ src/Arrays/Every Possible Combination/combinations.js
@@ -0,0 +1,11 @@
+/* 
+
+Generate every possible combination of characters, given a string or string iterable.
+  For given iterable ['a', 'b'] and desired length 2, it should output an 'aa' element.
+  Should throw on arrays too large.
+  
+*/
+
+export const ERROR_TOO_LARGE = 'Desired output too large!';
+
+export default function generateEveryPossibleCombination(iterable, length) {}
