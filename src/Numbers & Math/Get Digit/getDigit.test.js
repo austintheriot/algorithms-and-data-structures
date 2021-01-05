bb461a74278008bc3ae784c8382848f9e20c8e8b
Austin Theriot <austinmtheriot@gmail.com>
2021-01-05 11:57:24 -0600

    corrected variables & added describe

--- src/Numbers & Math/Get Digit/getDigit.test.js
+++ src/Numbers & Math/Get Digit/getDigit.test.js
@@ -1,22 +1,24 @@
-import getDecimal from './getDigit';
+import getDigit from './getDigit';
 
-it('Should return the digit at the indicated decimal index', () => {
-	expect(getDecimal(4321, 0)).toBe(1);
-	expect(getDecimal(4321, 1)).toBe(2);
-	expect(getDecimal(4321, 2)).toBe(3);
-	expect(getDecimal(4321, 3)).toBe(4);
-});
+describe('Get Digit', () => {
+	it('Should return the digit at the indicated decimal index', () => {
+		expect(getDigit(4321, 0)).toBe(1);
+		expect(getDigit(4321, 1)).toBe(2);
+		expect(getDigit(4321, 2)).toBe(3);
+		expect(getDigit(4321, 3)).toBe(4);
+	});
 
-it('Should work with negative numbers', () => {
-	expect(getDecimal(-4321, 0)).toBe(-1);
-	expect(getDecimal(-4321, 1)).toBe(-2);
-	expect(getDecimal(-4321, 2)).toBe(-3);
-	expect(getDecimal(-4321, 3)).toBe(-4);
-});
+	it('Should work with negative numbers', () => {
+		expect(getDigit(-4321, 0)).toBe(-1);
+		expect(getDigit(-4321, 1)).toBe(-2);
+		expect(getDigit(-4321, 2)).toBe(-3);
+		expect(getDigit(-4321, 3)).toBe(-4);
+	});
 
-it('Should return 0 for indices out of range', () => {
-	expect(getDecimal(4321, 4)).toBe(0);
-	expect(getDecimal(4321, -1)).toBe(0);
-	expect(getDecimal(-4321, 100)).toBe(0);
-	expect(getDecimal(-4321, -100)).toBe(0);
+	it('Should return 0 for indices out of range', () => {
+		expect(getDigit(4321, 4)).toBe(0);
+		expect(getDigit(4321, -1)).toBe(0);
+		expect(getDigit(-4321, 100)).toBe(0);
+		expect(getDigit(-4321, -100)).toBe(0);
+	});
 });
