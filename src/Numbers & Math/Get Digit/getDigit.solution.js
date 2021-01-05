8ef903cea065ed657f9149adfa5f9399ab35ba39
Austin Theriot <austinmtheriot@gmail.com>
2021-01-05 11:57:52 -0600

    added Math.sign() and changed to ESModules

--- src/Numbers & Math/Get Digit/getDigit.solution.js
+++ src/Numbers & Math/Get Digit/getDigit.solution.js
@@ -6,8 +6,7 @@ function countDigits(num) {
 	return num === 0 ? 1 : Math.floor(Math.log10(Math.abs(num))) + 1;
 }
 
-module.exports = function getDigit(num, i) {
+export default function getDigit(num, i) {
 	if (i < 0 || i > countDigits(num) - 1) return 0;
-	let sign = num < 0 ? -1 : 1;
-	return Math.floor((Math.abs(num) / 10 ** i) % 10) * sign;
-};
+	return Math.floor((Math.abs(num) / 10 ** i) % 10) * Math.sign(num);
+}
