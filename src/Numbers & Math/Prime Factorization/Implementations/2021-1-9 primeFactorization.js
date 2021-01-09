361afbe3128f248f4acb403d3557f19f2bae7ba1
Austin Theriot <austinmtheriot@gmail.com>
2021-01-09 09:47:36 -0600

    prime factorization practice

---
+++ src/Numbers & Math/Prime Factorization/Implementations/2021-1-9 primeFactorization.js
@@ -0,0 +1,30 @@
+/* 
+
+    Should store prime factors as a hash table of primes and their exponents
+    e.g. primeFactors(12345) = {
+      3:1,
+      5:1,
+      823:1
+    }; 
+
+    In other words, 12345 = (3 ^ 1) * (5 ^ 1) * (823 ^ 1);
+
+
+    Runtime complexity for this function is O(sqrt(n)), 
+    since the greatest prime factor of a number will only 
+    only ever be as high as that number's square root.
+    
+*/
+
+export default function primeFactors(num) {
+	const factors = {};
+	let divisor = 2;
+	while (num >= 2) {
+		if (num % divisor === 0) {
+			if (factors[divisor] == undefined) factors[divisor] = 0;
+			factors[divisor]++;
+			num /= divisor;
+		} else divisor++;
+	}
+	return factors;
+}
