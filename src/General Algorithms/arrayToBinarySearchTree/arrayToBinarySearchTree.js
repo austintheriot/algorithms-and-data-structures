e5dac56fdb6c63f1c5800f8a9ad87486ec07ec73
Austin Theriot <austinmtheriot@gmail.com>
2021-01-10 12:25:19 -0600

    implemented arrayToBinarySearchTree

---
+++ src/General Algorithms/arrayToBinarySearchTree/arrayToBinarySearchTree.js
@@ -0,0 +1,10 @@
+import Node from '../../Trees/Binary Tree/BinaryTreeNode.solution';
+
+/* 
+
+  Implement an algorithm that converts a sorted array into
+  a binary search tree with minimal height.
+
+*/
+
+export default function arrayToBinarySearchTree() {}
---
+++ src/General Algorithms/arrayToBinarySearchTree/arrayToBinarySearchTree.test.js
@@ -0,0 +1,10 @@
+import perfectBinarySearchTree from '../../Trees/Binary Tree/Examples/perfectBinarySearchTree';
+import arrayToTree from './arrayToBinarySearchTree';
+
+const array = [1, 3, 4, 5, 6, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18];
+
+describe('Array to Binary Search Tree', () => {
+	it('Should convert an array into a binary search tree with minimal height', () => {
+		expect(arrayToTree(array)).toEqual(perfectBinarySearchTree);
+	});
+});
