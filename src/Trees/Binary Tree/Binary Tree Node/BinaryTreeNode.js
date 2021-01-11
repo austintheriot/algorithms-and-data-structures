7f519ff7166c9bf9e2e568074ff74bf0d6b59120
Austin Theriot <austinmtheriot@gmail.com>
2021-01-11 12:11:43 -0600

    reset binary tree node file

--- src/Trees/Binary Tree/Binary Tree Node/BinaryTreeNode.js
+++ src/Trees/Binary Tree/Binary Tree Node/BinaryTreeNode.js
@@ -3,38 +3,4 @@
 // setRight(node){}
 // find
 
-export default class BinaryTreeNode {
-	constructor(data) {
-		this.parent = null;
-		this.data = data ?? null;
-		this.left = null;
-		this.right = null;
-	}
-
-	setLeft(node) {
-		//remove reference to parent for existing node
-		if (this.left) this.left.parent = null;
-		//set left node
-		this.left = node;
-		//link left node to parent
-		this.left.parent = this;
-		return this;
-	}
-
-	setRight(node) {
-		//remove reference to parent for existing node
-		if (this.right) this.right.parent = null;
-		//set right node
-		this.right = node;
-		//link right node to parent
-		this.right.parent = this;
-		return this;
-	}
-
-	find(data) {
-		if (data === this.data) return this;
-		else if (data < this.data) return this.left ? this.left.find(data) : null;
-		else if (data > this.data) return this.right ? this.right.find(data) : null;
-		return null;
-	}
-}
+export default class BinaryTreeNode {}
