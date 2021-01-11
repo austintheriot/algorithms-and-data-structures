71d2c4fa1432f5d56c3740167d6a819b4587033b
Austin Theriot <austinmtheriot@gmail.com>
2021-01-11 12:36:55 -0600

    changed parameter to use data rather than node

--- src/Trees/Binary Tree/Binary Search Tree Node/BinarySearchTreeNode.solution.js
+++ src/Trees/Binary Tree/Binary Search Tree Node/BinarySearchTreeNode.solution.js
@@ -1,13 +1,13 @@
 import BinaryTreeNode from '../Binary Tree Node/BinaryTreeNode.solution';
 
 export default class BinarySearchTreeNode extends BinaryTreeNode {
-	insertInOrder(node) {
-		if (node.data < this.data) {
-			if (!this.left) this.setLeft(node);
-			else this.left.insertNode(node);
-		} else if (node.data > this.data) {
-			if (!this.right) this.setRight(node);
-			else this.right.insertNode(node);
+	insertInOrder(data) {
+		if (data < this.data) {
+			if (!this.left) this.setLeft(new BinarySearchTreeNode(data));
+			else this.left.insertInOrder(new BinarySearchTreeNode(data));
+		} else if (data > this.data) {
+			if (!this.right) this.setRight(new BinarySearchTreeNode(data));
+			else this.right.insertInOrder(new BinarySearchTreeNode(data));
 		} else return this;
 	}
 }
