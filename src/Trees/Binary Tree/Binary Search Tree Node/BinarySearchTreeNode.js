fc0086d7511d77d4f3259bc0a377d37e40cd09ae
Austin Theriot <austinmtheriot@gmail.com>
2021-01-11 12:36:35 -0600

    reset file

--- src/Trees/Binary Tree/Binary Search Tree Node/BinarySearchTreeNode.js
+++ src/Trees/Binary Tree/Binary Search Tree Node/BinarySearchTreeNode.js
@@ -1,16 +1,5 @@
 import BinaryTreeNode from '../Binary Tree Node/BinaryTreeNode.solution';
 
-//implement the following methods:
 //insertInOrder
 
-export default class BinarySearchTreeNode extends BinaryTreeNode {
-	insertInOrder(node) {
-		if (node.data < this.data) {
-			if (!this.left) this.setLeft(node);
-			else this.left.insertInOrder(node);
-		} else if (node.data > this.data) {
-			if (!this.right) this.setRight(node);
-			else this.right.insertInOrder(node);
-		} else return this;
-	}
-}
+export default class BinarySearchTreeNode extends BinaryTreeNode {}
