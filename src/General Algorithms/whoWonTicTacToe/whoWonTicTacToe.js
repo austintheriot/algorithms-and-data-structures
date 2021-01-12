4372cfc5efcfaf4d073cbc7a99eb6601e9d17111
Austin Theriot <austinmtheriot@gmail.com>
2021-01-12 13:59:48 -0600

    clarified prompt

--- src/General Algorithms/whoWonTicTacToe/whoWonTicTacToe.js
+++ src/General Algorithms/whoWonTicTacToe/whoWonTicTacToe.js
@@ -1,7 +1,7 @@
 /* 
 
   Implement an algorithm to determine who, if anyone, has won a game of Tic Tac Toe.
-  Return 'x', 'o', 'draw', or 'unfinished'
+  Return 'x', 'o', 'draw', or 'unfinished' if no winner is found and the board is incomplete.
 
 */
 
