d282f57251b7dbce0852ed4a3796f28568d4e5a9
Austin Theriot <austinmtheriot@gmail.com>
2021-01-16 10:16:27 -0600

    cleaned up solution

--- src/Algorithms/Numbers & Math/sieveOfEratosthenes/Linear Time/sieveLinear.solution.js
+++ src/Algorithms/Numbers & Math/sieveOfEratosthenes/Linear Time/sieveLinear.solution.js
@@ -3,15 +3,15 @@
 //drawback is that it requires additionaly memory
 //of O(n) to store smallest prime factors
 
-export default function sieve(N) {
+export default function sieve(max) {
 	//smallest prime factor for of each index
 	//any number that remains with 0 is prime
-	const spf = new Array(N + 1).fill(0);
+	const spf = new Array(max + 1).fill(0);
 
 	//final array of prime numbers
 	const primes = [];
 
-	for (let i = 2; i <= N; i++) {
+	for (let i = 2; i <= max; i++) {
 		//if it is prime
 		if (spf[i] === 0) {
 			spf[i] = i;
@@ -24,11 +24,11 @@ export default function sieve(N) {
 			let j = 0;
 			j < primes.length && //iterate through array of found primes
 			primes[j] <= spf[i] && //only iterate primes up to the i's lowest prime
-			i * primes[j] <= N; //do not check beyond max
+			primes[j] * i <= max; //do not check beyond max
 			j++
 		) {
 			//set the smallest prime factor of the multiple
-			spf[i * primes[j]] = primes[j]; //MUST assign primes[j], or else the spf[] value will not be the true lowest prime factor
+			spf[primes[j] * i] = primes[j]; //MUST assign primes[j], or else the spf[] value will not be the true lowest prime factor
 		}
 	}
 	return primes;
