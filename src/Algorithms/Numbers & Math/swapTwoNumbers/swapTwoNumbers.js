1b2dda99bd1e900199716a577ba7e9b563ac8597
Austin Theriot <austinmtheriot@gmail.com>
2021-01-16 09:59:42 -0600

    reset file

--- src/Algorithms/Numbers & Math/swapTwoNumbers/swapTwoNumbers.js
+++ src/Algorithms/Numbers & Math/swapTwoNumbers/swapTwoNumbers.js
@@ -6,8 +6,5 @@
 */
 
 export default function swapTwoNumbers(arr) {
-	arr[0] = arr[0] + arr[1]; //get total
-	arr[1] = arr[0] - arr[1]; //element 0 is now 1 (total - 1)
-	arr[0] = arr[0] - arr[1]; //element 1 s now 0 (total - 1)
 	return arr;
 }
