26df7fcf73ed07187120304e762972dfbddb5be9
Austin Theriot <austinmtheriot@gmail.com>
2021-01-16 10:44:21 -0600

    intersection of two arrays practice

--- src/Algorithms/Arrays/intersectionOfTwoArrays/intersectionOfTwoArrays.js
+++ src/Algorithms/Arrays/intersectionOfTwoArrays/intersectionOfTwoArrays.js
@@ -5,4 +5,19 @@
 
 */
 
-export default function intersectionOfTwoArrays(array1, array2) {}
+export default function intersectionOfTwoArrays(a1, a2) {
+	const intersection = [];
+	a1 = a1.sort((a, b) => a - b);
+	a2 = a2.sort((a, b) => a - b);
+
+	for (let i1 = 0, i2 = 0; i1 < a1.length && i2 < a2.length; ) {
+		if (a1[i1] < a2[i2]) i1++;
+		else if (a2[i2] < a1[i1]) i2++;
+		else if (a1[i1] === a2[i2]) {
+			intersection.push(a1[i1]);
+			i1++, i2++;
+		}
+	}
+
+	return intersection;
+}
