5c405cef8d4b1ed9644ef0083b8b81b07ee5a6e0
Austin Theriot <austinmtheriot@gmail.com>
2021-01-17 11:06:15 -0600

    updated to use older version of comparison tree

--- src/Algorithms/Trees/arrayToBinarySearchTree/arrayToBinarySearchTree.test.js
+++ src/Algorithms/Trees/arrayToBinarySearchTree/arrayToBinarySearchTree.test.js
@@ -1,4 +1,4 @@
-import perfectBinarySearchTree from '../../../Data Structures/Trees/Binary Tree/Examples/perfectBinarySearchTree';
+import perfectBinarySearchTree from '../../../Data Structures/Trees/Binary Tree/Examples/perfectBinarySearchTree (old)';
 import arrayToTree from './arrayToBinarySearchTree';
 
 const array = [1, 3, 4, 5, 6, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18];
