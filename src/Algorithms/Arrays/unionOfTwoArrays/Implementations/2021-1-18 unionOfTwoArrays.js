ed9d03a21d976cc39be118bf71d1ba86ebd76476
Austin Theriot <austinmtheriot@gmail.com>
2021-01-18 07:49:45 -0600

    union of two arrays practice

---
+++ src/Algorithms/Arrays/unionOfTwoArrays/Implementations/2021-1-18 unionOfTwoArrays.js
@@ -0,0 +1,25 @@
+/* 
+
+  Implement an algorithm that returns the union 
+  of two arrays (eliminating duplicates in the process).
+  The arrays are not necessarily sorted.
+
+*/
+
+export default function unionOfTwoArrays(r1, r2) {
+	const hashTable = {};
+	const union = [];
+	r1.forEach((el) => {
+		if (!hashTable[el]) {
+			hashTable[el] = true;
+			union.push(el);
+		}
+	});
+	r2.forEach((el) => {
+		if (!hashTable[el]) {
+			hashTable[el] = true;
+			union.push(el);
+		}
+	});
+	return union;
+}
