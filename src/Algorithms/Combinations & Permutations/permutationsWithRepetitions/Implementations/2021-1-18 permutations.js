ba6805727daa9dc8c37d2fe4fb9e225aa3d60935
Austin Theriot <austinmtheriot@gmail.com>
2021-01-18 07:41:05 -0600

    permutations with repetitions practice

---
+++ src/Algorithms/Combinations & Permutations/permutationsWithRepetitions/Implementations/2021-1-18 permutations.js
@@ -0,0 +1,32 @@
+/* 
+
+Generate every possible combination of characters, given a string or string iterable.
+  For given iterable ['a', 'b'] and desired length 2, it should output an 'aa' element.
+  Should throw on arrays too large.
+  
+*/
+
+//optional
+import Queue from '../../../Data Structures/Queues/Queue.solution';
+export const ERROR_TOO_LARGE = 'Desired output too large!';
+export const MAX_ARRAY_LENGTH = 2 ** 32 - 1;
+
+export default function permutations(iterable, length) {
+	if (length === 0) return [];
+	if (iterable.length ** length > MAX_ARRAY_LENGTH)
+		throw new Error(ERROR_TOO_LARGE);
+
+	let storage = [...iterable];
+
+	while (length > 1) {
+		const newStorage = [];
+		for (let i = 0; i < storage.length; i++) {
+			for (let j = 0; j < iterable.length; j++) {
+				newStorage.push(storage[i] + iterable[j]);
+			}
+		}
+		storage = newStorage;
+		length--;
+	}
+	return storage;
+}
