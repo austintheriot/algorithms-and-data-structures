94497b78dcb5187bcb76ad7fbbf4c7993a422901
Austin Theriot <austinmtheriot@gmail.com>
2021-01-18 07:14:16 -0600

    corrected solution

--- src/Data Structures/Trees/Binary Tree/Binary Search Tree Node/BinarySearchTreeNode.solution.js
+++ src/Data Structures/Trees/Binary Tree/Binary Search Tree Node/BinarySearchTreeNode.solution.js
@@ -4,10 +4,10 @@ export default class BinarySearchTreeNode extends BinaryTreeNode {
 	insertInOrder(data) {
 		if (data < this.data) {
 			if (!this.left) this.setLeft(new BinarySearchTreeNode(data));
-			else this.left.insertInOrder(new BinarySearchTreeNode(data));
+			else this.left.insertInOrder(data);
 		} else if (data > this.data) {
 			if (!this.right) this.setRight(new BinarySearchTreeNode(data));
-			else this.right.insertInOrder(new BinarySearchTreeNode(data));
+			else this.right.insertInOrder(data);
 		} else return this;
 	}
 }
