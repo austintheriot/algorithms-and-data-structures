702204a6fd664bef66e32a4e964d7e548f19451e
Austin Theriot <austinmtheriot@gmail.com>
2021-01-18 07:32:24 -0600

    array to binary search tree practice

---
+++ src/Algorithms/Trees/arrayToBinarySearchTree/Implementations/2021-1-18 arrayToBinarySearchTree.js
@@ -0,0 +1,26 @@
+import Node from '../../../Data Structures/Trees/Binary Tree/Binary Search Tree Node/BinarySearchTreeNode.solution';
+
+/* 
+
+  Implement an algorithm that converts a sorted array into
+  a binary search tree with minimal height.
+
+*/
+
+export default function arrayToBinarySearchTree(array, low, high) {
+	low = low ?? 0;
+	high = high ?? array.length - 1;
+
+	if (low > high) return;
+
+	const middle = Math.floor(low + (high - low) / 2);
+	const node = new Node(array[middle]);
+
+	const leftBranch = arrayToBinarySearchTree(array, low, middle - 1);
+	if (leftBranch) node.setLeft(leftBranch);
+
+	const rightBranch = arrayToBinarySearchTree(array, middle + 1, high);
+	if (rightBranch) node.setRight(rightBranch);
+
+	return node;
+}
