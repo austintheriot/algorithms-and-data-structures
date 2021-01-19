467c2b33ff72bb3b8952d38f8beb9e387b014e75
Austin Theriot <austinmtheriot@gmail.com>
2021-01-19 07:28:25 -0600

    reverse doubly linked list practice

---
+++ src/Algorithms/Linked Lists/reverseDoublyLinkedList/Implementations/2021-1-19 reverseDoublyLinkedList.js
@@ -0,0 +1,16 @@
+/* 
+
+  Given the first node of a doubly linked list,
+  implement an algorithm to reverse the linked list in place.
+
+*/
+
+export default function reverseDoublyLinkedList(node) {
+  let current = node;
+  while (current) {
+    const nextNode = current.next;
+    current.next = current.prev;
+    current = current.prev = nextNode;
+  }
+  return current;
+}
