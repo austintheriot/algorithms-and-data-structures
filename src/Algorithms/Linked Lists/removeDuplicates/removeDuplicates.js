d6d5f621e9e842805ec7d9f1c9dd8a7d76ecb24f
Austin Theriot <austinmtheriot@gmail.com>
2021-01-20 08:05:49 -0600

    reset file

--- src/Algorithms/Linked Lists/removeDuplicates/removeDuplicates.js
+++ src/Algorithms/Linked Lists/removeDuplicates/removeDuplicates.js
@@ -5,39 +5,4 @@
 
 */
 
-//with hash table
-function removeDuplicates1(linkedList) {
-	const hashTable = {};
-	let currentNode = linkedList.head;
-	hashTable[currentNode.data] = true;
-
-	while (currentNode && currentNode.next) {
-		if (hashTable[currentNode.next.data]) {
-			currentNode.next = currentNode.next.next;
-			linkedList.length--;
-		} else {
-			hashTable[currentNode.next.data] = true;
-			currentNode = currentNode.next;
-		}
-	}
-	return linkedList;
-}
-
-//without a hash table
-export default function removeDuplicates2(linkedList) {
-	let current = linkedList.head;
-	while (current) {
-		let runner = current;
-		while (runner.next) {
-			if (current.data === runner.next.data) {
-				runner.next = runner.next.next;
-				linkedList.length--;
-			} else {
-				runner = runner.next;
-			}
-		}
-		current = current.next;
-	}
-
-	return linkedList;
-}
+export default function removeDuplicates(linkedList) {}
