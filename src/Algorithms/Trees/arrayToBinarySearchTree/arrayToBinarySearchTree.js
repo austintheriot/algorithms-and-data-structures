92966eff017799b2bba719befda44547a295f2d7
Austin Theriot <austinmtheriot@gmail.com>
2021-01-22 17:00:57 -0600

    practice array to binary search tree

--- src/Algorithms/Trees/arrayToBinarySearchTree/arrayToBinarySearchTree.js
+++ src/Algorithms/Trees/arrayToBinarySearchTree/arrayToBinarySearchTree.js
@@ -1,4 +1,4 @@
-import Node from '../../../Data Structures/Trees/Binary Tree/Binary Search Tree Node/BinarySearchTreeNode.solution';
+import Node from "../../../Data Structures/Trees/Binary Tree/Binary Search Tree Node/BinarySearchTreeNode.solution";
 
 /* 
 
