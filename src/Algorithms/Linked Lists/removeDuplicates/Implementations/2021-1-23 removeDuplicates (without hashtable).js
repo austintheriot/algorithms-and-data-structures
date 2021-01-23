265a163fc529f80606f3b4cad2cdfe9dbaf5082b
Austin Theriot <austinmtheriot@gmail.com>
2021-01-23 11:37:11 -0600

    practice removing duplicates from linked list

---
+++ src/Algorithms/Linked Lists/removeDuplicates/Implementations/2021-1-23 removeDuplicates (without hashtable).js
@@ -0,0 +1,23 @@
+/* 
+
+  Implement an algorithm to remove the duplicates from a singly linked list.
+	(Optionally: do so without any secondary data structure)
+
+*/
+
+export default function removeDuplicates(linkedList) {
+  let current = linkedList.head;
+  while (current) {
+    let runner = current;
+    while (runner.next) {
+      if (runner.next.data === current.data) {
+        runner.next = runner.next.next;
+        linkedList.length--;
+      } else {
+        runner = runner.next;
+      }
+    }
+    current = current.next;
+  }
+  return linkedList;
+}
