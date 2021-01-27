f8a4afc254cbe793cd882801dcfda0a49bfc841d
Austin Theriot <austinmtheriot@gmail.com>
2021-01-27 06:45:05 -0600

    add second solution

--- src/Algorithms/Strings/checkPermutation/checkPermutation.solution.js
+++ src/Algorithms/Strings/checkPermutation/checkPermutation.solution.js
@@ -4,22 +4,36 @@
 
 */
 
+//with hash table
 export default function checkPermutation(string1, string2) {
-	if (string1.length !== string2.length) return false;
+  if (string1.length !== string2.length) return false;
 
-	const hashTable = {};
+  const hashTable = {};
 
-	for (let i = 0; i < string1.length; i++) {
-		const ch = string1[i];
-		if (!hashTable[ch]) hashTable[ch] = 0;
-		hashTable[ch]++;
-	}
+  for (let i = 0; i < string1.length; i++) {
+    const ch = string1[i];
+    if (!hashTable[ch]) hashTable[ch] = 0;
+    hashTable[ch]++;
+  }
 
-	for (let i = 0; i < string2.length; i++) {
-		const ch = string2[i];
-		if (!hashTable[ch]) return false; //if character not defined
-		hashTable[ch]--;
-		if (hashTable[ch] < 0) return false; //if character count less than 0;
-	}
-	return true;
+  for (let i = 0; i < string2.length; i++) {
+    const ch = string2[i];
+    if (!hashTable[ch]) return false; //if character not defined
+    hashTable[ch]--;
+    if (hashTable[ch] < 0) return false; //if character count less than 0;
+  }
+  return true;
+}
+
+//without hashtable
+function checkPermutation(s1, s2) {
+  s1 = s1.split("").sort();
+  s2 = s2.split("").sort();
+
+  for (let i1 = 0, i2 = 0; i1 < s1.length && i2 < s2.length; ) {
+    if (s1[i1] !== s2[i2]) return false;
+    else i1++, i2++;
+  }
+
+  return true;
 }
