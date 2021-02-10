e7f224a4fea73fab426c0a1de49c7753c4c4361b
Austin Theriot <austinmtheriot@gmail.com>
2021-02-09 20:44:34 -0600

    practice sieve

---
+++ src/Algorithms/Numbers & Math/sieveOfEratosthenes/Standard/Implementations/2021-2-9 sieve.js
@@ -0,0 +1,18 @@
+export default function sieve(max) {
+  const primes = [];
+  const isPrime = new Array(max + 1).fill(true);
+
+  isPrime[0] = isPrime[1] = false;
+
+  for (let i = 2; i <= max; i++){
+    if (isPrime[i]) {
+      primes.push(i);
+
+      for (let j = i ** 2; j <= max; j += i){
+        isPrime[j] = false;
+      }
+    }
+  }
+
+  return primes;
+}
