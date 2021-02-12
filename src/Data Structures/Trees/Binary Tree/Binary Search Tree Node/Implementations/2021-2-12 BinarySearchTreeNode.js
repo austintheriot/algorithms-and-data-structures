6a773514daa8d23e79d708270e9e7d07fd538e7d
Austin Theriot <austinmtheriot@gmail.com>
2021-02-12 08:28:23 -0600

    practice binary search tree node

---
+++ src/Data Structures/Trees/Binary Tree/Binary Search Tree Node/Implementations/2021-2-12 BinarySearchTreeNode.js
@@ -0,0 +1,12 @@
+import BinaryTreeNode from "../Binary Tree Node/BinaryTreeNode.solution";
+
+//insertInOrder(data){}
+
+export default class BinarySearchTreeNode extends BinaryTreeNode {
+  insertInOrder(data) {
+    const node = new BinarySearchTreeNode(data);
+    if (data <= this.data)  this.left ? this.left.insertInOrder(data) : this.setLeft(node);
+    if (data > this.data) this.right ? this.right.insertInOrder(data) : this.setRight(node);
+  }
+}
+s
