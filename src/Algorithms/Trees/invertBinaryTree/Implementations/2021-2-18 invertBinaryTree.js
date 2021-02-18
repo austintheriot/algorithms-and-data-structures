63cc86cde5011939477ff6211e01bbee4b0745d7
Austin Theriot <austinmtheriot@gmail.com>
2021-02-18 08:53:20 -0600

    practice invert binary tree

---
+++ src/Algorithms/Trees/invertBinaryTree/Implementations/2021-2-18 invertBinaryTree.js
@@ -0,0 +1,12 @@
+/* 
+  Given the root node of a binary tree,
+  implement an algorithm to flip the tree horizontally
+  (invert the binary tree).
+*/
+
+export default function invert(node) {
+  if (!node) return;
+  const left = node.left;
+  node.left = node.right, node.right = left;
+  invert(node.left), invert(node.right);
+}
